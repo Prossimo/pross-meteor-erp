f8737db1acabad1dab726c74ed0b64e64a73b978
Ismail Frolov <i.f.0109@hotmail.com>
2017-02-22 17:23:28 +0200

    Fix InboxPage layout

--- imports/ui/pages/InboxPage.jsx
+++ imports/ui/pages/InboxPage.jsx
@@ -9,17 +9,17 @@ class Inbox extends React.Component{
     render() {
 
         return (
-            <div className="page-container admin-page">
-                <div className="main-content">
-                    <div className="tab-container">
-                        <h2 className="page-title">Inbox page</h2>
-
+            <div className="inbox-page">
+                <h2 className="page-title">Inbox page</h2>
+                <div className="content-panel">
+                    <div className="column-panel" style={{order:1, minWidth:150, maxWidth:150, borderRight:'1px solid rgba(221,221,221,0.6)'}}>
+                        <div style={{position:'relative',display:'flex',flexDirection:'column',height:'100%'}}>
+                        Folder panel
+                        </div>
                     </div>
+                    <div className="column-panel" style={{order:2, minWidth:250, maxWidth:450, borderRight:'1px solid rgba(221,221,221,0.6)'}}>Thread panel</div>
+                    <div className="column-panel" style={{order:3, flex:1}}>View panel</div>
                 </div>
-                <aside className="right-sidebar">
-                    <h2 className="title">Inbox</h2>
-
-                </aside>
             </div>
         )
     }
