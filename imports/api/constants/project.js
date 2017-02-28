9205efc4fe31a16c97f7d86287b954ad6f9b9329
oleksiy <frayeralex@gmail.com>
2017-02-28 23:01:19 +0200

    add fields to CreateProject form; add Project Details tab with general information; fix app style;

--- imports/api/constants/project.js
+++ imports/api/constants/project.js
@@ -1,2 +1,3 @@
 export const DESIGNATION_LIST = ['Standart', 'Guest'];
+export const SHIPPING_MODE_LIST = ['LCL', 'FCL', 'FCL Pallets', 'Courrier'];
 export const STAKEHOLDER_CATEGORY = ['Architect', 'Developers', 'GC', 'Contractor', 'Installer', 'Owner', 'Consultant'];
--- imports/ui/components/project/AllProjects.jsx
+++ imports/ui/components/project/AllProjects.jsx
@@ -4,38 +4,24 @@ import classNames from 'classnames';
 class AllProjects extends React.Component{
     constructor(props){
         super(props);
-        this.state = {
-            active: true,
-            delivered: false
-        }
-    }
-
-    toggleState(state){
-        this.setState({[state]: !this.state[state]});
     }
     
     renderProjectList(){
-        const { active, delivered } = this.state;
         const { projects } = this.props;
-        const projectToShow = projects.filter((item)=>{
-            if(!active && !delivered) return false;
-            if(active && delivered) return true;
-            if(!delivered && active == item.active)return true;
-            if(!active && delivered == !item.active)return true;
-        });
 
         return (
             <ul className="project-list">
-                {projectToShow.map(project=>{
+                {projects.map(project=>{
                     return(
                         <li key={project._id}
                             onClick={this.goToProject.bind(this, project)}
                             className={classNames("project-item", {"slack-integrate": !!project.slackChanel})}>
                             <div className="left-part">
                                 <p className="title">{project.name}</p>
-                                <p className={classNames("status", {[project.status]: project.status})}>{project.status}</p>
                             </div>
-                            <div className="right-part"></div>
+                            <div className="right-part">
+
+                            </div>
                         </li>
                     )
                 })}
@@ -48,17 +34,8 @@ class AllProjects extends React.Component{
     }
 
     render() {
-    	  const { active, delivered } = this.state;
         return (
-           <div className=""> 
-           		<div className="controls-panel">
-           			<ul>
-                        <li className={classNames("control-item", {"non-active": !active})}
-                            onClick={this.toggleState.bind(this, "active")}>Active</li>
-                        <li className={classNames("control-item", {"non-active": !delivered})}
-                            onClick={this.toggleState.bind(this, "delivered")}>Delivered</li>
-                    </ul>
-           		</div>
+           <div className="">
                {this.renderProjectList()}
            </div>
 		  )
