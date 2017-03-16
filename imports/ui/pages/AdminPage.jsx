897cf5f471405dd1c8c63fce92f1d20db411cf32
Vitaliy <vitaltrachenko@gmail.com>
2017-03-16 19:19:28 +0200

    full merge with dev branch

--- imports/ui/pages/AdminPage.jsx
+++ imports/ui/pages/AdminPage.jsx
@@ -1,58 +1,5 @@
 import React from 'react';
-import classNames from 'classnames';
-import { Button, Table, Modal } from 'react-bootstrap';
-
-import CreateUser from '../components/admin/CreateUser';
-import CreateProject from '../components/admin/CreateProject';
-import EditUser  from '../components/admin/EditUser'
-import RemoveUser  from '../components/admin/RemoveUser'
-
-class CreateUserModal extends React.Component{
-  constructor(props){
-    super(props);
-    
-    this.state = {
-      showModal: false
-    };
-    
-    this.handleModalState = this.handleModalState.bind(this);
-    this.close = this.close.bind(this);
-    this.open = this.open.bind(this);
-  }
-  
-  close() {
-    this.setState({ showModal: false });
-  }
-  
-  open() {
-    this.setState({ showModal: true });
-  }
-  
-  handleModalState () {
-    this.setState({
-      showModal: false
-    });
-  }
-  
-  render() {
-    return (
-      <div>
-        <button className="btnn login-btn" onClick={this.open}>Create new user</button>
-        <Modal show={this.state.showModal} onHide={this.close}>
-          <Modal.Header closeButton>
-            <Modal.Title>Create user</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <CreateUser updateModalState={this.handleModalState}/>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button onClick={this.close}>Close</Button>
-          </Modal.Footer>
-        </Modal>
-      </div>
-    );
-  }
-}
+import EditableUsersTable  from '../components/admin/EditableUsersTable'
 
 class AdminPage extends React.Component{
   constructor(props){
@@ -65,40 +12,7 @@ class AdminPage extends React.Component{
         <div className="main-content">
           <div className="tab-container">
             <h2 className="page-title">Admin page</h2>
-            <CreateUserModal />
-            <Table striped bordered condensed hover>
-              <thead>
-              <tr>
-                <th>#</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Username</th>
-                <th>Email</th>
-                <th>Status</th>
-                <th>Role</th>
-                <th>Actions</th>
-              </tr>
-              </thead>
-              {this.props.createdUsers.map((user, index) =>{
-                return(
-                  <tbody key={user._id}>
-                  <tr>
-                    <td>{index + 1}</td>
-                    <td>{user.firstName}</td>
-                    <td>{user.lastName}</td>
-                    <td>{user.username}</td>
-                    <td>{user.email}</td>
-                    <td>{user.isActive ? "Active" : "Not active yet"}</td>
-                    <td>{user.role}</td>
-                    <td>
-                      <EditUser user={user}/>
-                      <RemoveUser userId={user._id} />
-                    </td>
-                  </tr>
-                  </tbody>
-                )
-              })}
-            </Table>
+            <EditableUsersTable createdUsers={this.props.createdUsers}/>
           </div>
         </div>
       </div>
