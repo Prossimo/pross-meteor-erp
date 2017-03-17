a3b3f3e846d8cbf2591035a922f2617365fcdf37
Duy Tai Nguyen <duytai.cse@gmail.com>
2017-03-17 15:56:26 +0700

    add tabs to top menus of stage

--- imports/ui/pages/LeadsPage.jsx
+++ imports/ui/pages/LeadsPage.jsx
@@ -1,17 +1,65 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
 import AllProjects from '../components/project/AllProjects';
+import CreateProject from '/imports/ui/components/admin/CreateProject';
 
 class LeadsPage extends React.Component{
     constructor(props){
         super(props);
+        this.tabs = [
+            {
+                label: 'All Leads',
+                component: <AllProjects/>
+            },
+            {
+                label: 'Add Lead',
+                component: <CreateProject/>
+            }
+        ];
+        this.state ={
+            activeTab: this.tabs[0]
+        }
+        this.getTabs = this.getTabs.bind(this);
+        this.toggleTab = this.toggleTab.bind(this);
+        this.getContent = this.getContent.bind(this);
+    }
+
+     getTabs(){
+        const { activeTab } = this.state;
+        return <ul>
+            {this.tabs.map(item=>{
+                return (
+                    <li key={item.label}
+                        onClick={this.toggleTab.bind(this, item)}
+                        className={classNames({'active': item === activeTab})}
+                    >{item.label}</li>
+                )
+            })}
+        </ul>
+    }
+
+    toggleTab(activeTab){
+        this.setState({activeTab})
+    }
+
+    getContent(){
+        const { activeTab } = this.state;
+        if(!activeTab.component) return null;
+        const salesRecords = this.props.salesRecords.filter(({ stage })=> stage === 'lead');
+        return React.cloneElement(<AllProjects/>, { salesRecords });
     }
 
     render() {
-        const salesRecords = this.props.salesRecords.filter(({ stage })=> stage === 'lead');
-        const LeadSalesRecords = React.cloneElement(<AllProjects/>, { salesRecords });
         return (
-            <div className="leads-page">
-                { LeadSalesRecords }
+            <div className='leads-page'>
+                <div className='tab-container'>
+                    <div className='tab-controls'>
+                        {this.getTabs()}
+                    </div>
+                    <div className='tab-content'>
+                        {this.getContent()}
+                    </div>
+                </div>
             </div>
         )
     }
--- imports/ui/pages/OrdersPage.jsx
+++ imports/ui/pages/OrdersPage.jsx
@@ -1,18 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
+import classNames from 'classnames';
 import AllProjects from '../components/project/AllProjects';
+import CreateProject from '/imports/ui/components/admin/CreateProject';
 
 class OrdersPage extends React.Component{
     constructor(props){
         super(props);
+        this.tabs = [
+            {
+                label: 'All Orders',
+                component: <AllProjects/>
+            },
+            {
+                label: 'Add Order',
+                component: <CreateProject/>
+            }
+        ];
+        this.state ={
+            activeTab: this.tabs[0]
+        }
+        this.getTabs = this.getTabs.bind(this);
+        this.toggleTab = this.toggleTab.bind(this);
+        this.getContent = this.getContent.bind(this);
+    }
 
+     getTabs(){
+        const { activeTab } = this.state;
+        return <ul>
+            {this.tabs.map(item=>{
+                return (
+                    <li key={item.label}
+                        onClick={this.toggleTab.bind(this, item)}
+                        className={classNames({'active': item === activeTab})}
+                    >{item.label}</li>
+                )
+            })}
+        </ul>
+    }
+
+    toggleTab(activeTab){
+        this.setState({activeTab})
+    }
+
+    getContent(){
+        const { activeTab } = this.state;
+        if(!activeTab.component) return null;
+        const salesRecords = this.props.salesRecords.filter(({ stage })=> stage === 'order');
+        return React.cloneElement(<AllProjects/>, { salesRecords });
     }
 
     render() {
-        const salesRecords = this.props.salesRecords.filter(({ stage })=> stage === 'order');
-        const orderSalesRecords = React.cloneElement(<AllProjects/>, { salesRecords });
         return (
-            <div className="orders-page">
-                { orderSalesRecords }
+            <div className='orders-page'>
+                <div className='tab-container'>
+                    <div className='tab-controls'>
+                        {this.getTabs()}
+                    </div>
+                    <div className='tab-content'>
+                        {this.getContent()}
+                    </div>
+                </div>
             </div>
         )
     }
