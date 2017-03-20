d4ca74edda62a5d3b9e9f4edf18a596ce2e81b38
Obulten <obulten22@gmail.com>
2017-03-20 15:58:47 +0100

    move project under contacts

--- imports/ui/components/aside/Aside.jsx
+++ imports/ui/components/aside/Aside.jsx
@@ -136,7 +136,7 @@ class Aside extends React.Component{
         }
         //admin & super admin allow
         if(Roles.userIsInRole( props.currentUser._id, [...ADMIN_ROLE_LIST] )){
-            this.pages.unshift({
+            this.pages.splice(4,0,{
                 label: "Projects",
                 topLevel: true,
                 subItems: [],
