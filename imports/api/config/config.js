691ebd67df615c00376b7746d3d8c4db458b6f31
Vitaliy <vitaltrachenko@gmail.com>
2017-03-22 20:31:14 +0200

    add google api cert

--- imports/api/config/config.js
+++ imports/api/config/config.js
@@ -9,9 +9,9 @@ module.exports = {
     google: {
         clientId: "949141736454-1ikg5jdl1iela2q1ck7lrhgghbpke43l.apps.googleusercontent.com",
         clientSecret: "E_Iahm05NOj9RxgHhelxRo9S",
+        redirectUri: `${Meteor.absoluteUrl()}auth/google/callback`,
         clientDriveId: '977294428736-it2160rgkpdtrp4hjm3mhjr97n4q5rmp.apps.googleusercontent.com',
         clientDriveSecret: 'aJmwh_egnDWiuebEc2GkEwOA',
-        redirectUri: `${Meteor.absoluteUrl()}auth/google/callback`,
         serviceAccountEmail: '977294428736-compute@developer.gserviceaccount.com',
         serviceAccountPemCertPath: `${Meteor.absolutePath}/prossimo-us.pem`
     },
