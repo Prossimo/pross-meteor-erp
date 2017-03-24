afba6f6b08048878b4e890f46cea367f9fa92ba8
Ismail Frolov <i.f.0109@hotmail.com>
2017-03-24 20:55:58 +0200

    Add toolbar and overview to contact page

--- imports/ui/components/contacts/ContactsList.js
+++ imports/ui/components/contacts/ContactsList.js
@@ -1,9 +1,12 @@
 import React from 'react'
-import {Table} from 'react-bootstrap'
+import {Button, Table, InputGroup, FormControl} from 'react-bootstrap'
 import ContactStore from '../../../api/nylas/contact-store'
 
-export default class extends React.Component {
-    static propTypes = {}
+
+export default class ContactsList extends React.Component {
+    static propTypes = {
+        onSelectContact: React.PropTypes.func
+    }
 
     constructor(props) {
         super(props)
@@ -27,36 +30,63 @@ export default class extends React.Component {
 
     render() {
         return (
-            <Table striped hover>
-                <thead>
-                <tr>
-                    <th width="5%">#</th>
-                    <th width="20%">Name</th>
-                    <th width="25%">Email</th>
-                    <th width="15%">Phone Numbers</th>
-                    <th width="25%">Description</th>
-                    <th width="10%">Inbox</th>
-                </tr>
-                </thead>
-                <tbody onScroll={this.onScrollContactList}>
-                {this.renderItems()}
-                </tbody>
-            </Table>
+            <div className="contact-list">
+                {this.renderToolbar()}
+
+                {this.renderContent()}
+            </div>
         )
     }
 
-    renderItems() {
+    renderToolbar() {
+        return (
+            <div className="toolbar-panel">
+                <div style={{flex: 1}}>
+                    <Button bsStyle="primary"><i className="fa fa-user-plus"/></Button>
+                </div>
+                <div style={{width:250}}>
+                    <InputGroup>
+                        <InputGroup.Addon><i className="fa fa-search"/></InputGroup.Addon>
+                        <FormControl type="text" placeholder="Search..." onChange={this.onChangeSearch} />
+                    </InputGroup>
+                </div>
+            </div>
+        )
+    }
+
+    renderContent() {
+        return (
+            <div className="content-panel">
+                <Table striped hover>
+                    <thead>
+                    <tr>
+                        <th width="5%">#</th>
+                        <th width="20%">Name</th>
+                        <th width="25%">Email</th>
+                        <th width="15%">Phone Numbers</th>
+                        <th width="25%">Description</th>
+                        <th width="10%">Inbox</th>
+                    </tr>
+                    </thead>
+                    <tbody onScroll={this.onScrollContactList}>
+                    {this.renderContacts()}
+                    </tbody>
+                </Table>
+            </div>
+        )
+    }
+    renderContacts() {
         const {contacts} = this.state
 
         if (!contacts || contacts.length == 0) return ''
 
         compare = (c1, c2) => {
-            if(c1.name > c2.name) return 1
-            else if(c1.name < c2.name) return -1
+            if (c1.name > c2.name) return 1
+            else if (c1.name < c2.name) return -1
             return 0
         }
         return contacts.sort(compare).map((contact, index) => (
-            <tr key={contact._id}>
+            <tr key={contact._id} onClick={()=>{this.props.onSelectContact && this.props.onSelectContact(contact)}}>
                 <td width="5%">{index + 1}</td>
                 <td width="20%">{contact.name}</td>
                 <td width="25%">{contact.email}</td>
@@ -73,11 +103,11 @@ export default class extends React.Component {
 
         if (!ContactStore.fullyLoaded && el.scrollTop + el.clientHeight == el.scrollHeight) {
 
-            if (this.timeout) {
-                clearTimeout(this.timeout);
+            if (this.scrollTimeout) {
+                clearTimeout(this.scrollTimeout);
             }
 
-            this.timeout = setTimeout(() => {
+            this.scrollTimeout = setTimeout(() => {
 
                 this.setState({contacts: ContactStore.getContacts({page: ContactStore.currentPage + 1})})
 
@@ -87,5 +117,15 @@ export default class extends React.Component {
         }
     }
 
+    onChangeSearch = (evt) => { console.log('onChangeSearch', evt.target.value)
+        if(this.searchTimeout) { clearTimeout(this.searchTimeout); }
+
+        const keyword = evt.target.value
+        this.searchTimeout = setTimeout(() => {
+            ContactStore.searchContacts(keyword, {limit:100}).then((contacts)=> {
+                this.setState({contacts: contacts})
+            })
+        }, 500)
+    }
 }
 
--- imports/ui/pages/ContactsPage.jsx
+++ imports/ui/pages/ContactsPage.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import ContactsList from '../components/contacts/ContactsList'
+import ContactOverview from '../components/contacts/ContactOverview'
 
 export default class ContactsPage extends React.Component{
     constructor(props){
         super(props);
 
+        this.state = {
+            selectedContact: null
+        }
     }
 
     render() {
         return (
-            <div className="contacts-page">
-                <ContactsList/>
+            <div className="contact-page">
+                <ContactsList onSelectContact={(contact)=>this.setState({selectedContact:contact})}/>
+                <ContactOverview contact={this.state.selectedContact}/>
             </div>
         )
     }
