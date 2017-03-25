f6842fdc1a13c1e91adae833d8a9df2ce30f2f41
bstarr322 <a@Jakobs-iMac.local>
2017-03-25 08:06:55 +0100

    add todo model

---
+++ imports/api/models/todos/methods.js
@@ -0,0 +1,63 @@
+import _ from 'underscore';
+import {Todos} from './todos'
+
+Meteor.methods({
+    insertTodo(data)
+    {
+        check(data, {
+            id: String,
+            account_id: String,
+            email: String,
+            title: Match.Maybe(String),
+            content: Match.Maybe(String)
+        });
+
+        return Todos.insert(data)
+    },
+
+    updateTodo(id, data)
+    {
+        check(data, {
+            account_id: Match.Maybe(String),
+            email: Match.Maybe(String),
+            title: Match.Maybe(String),
+            content: Match.Maybe(String)
+        });
+
+        return Todos.update({id}, data)
+    },
+
+    insertOrUpdateTodos(data) {
+        if (!data || data.length == 0) return null
+
+        data = data.filter((item) => item.id && item.email && item.account_id)
+
+        let existingTodos = Todos.find({id: {$in: _.pluck(data, 'id')}}).fetch()
+
+        let ids = []
+        data.forEach((item) => {
+            const {id, title} = item
+
+            let todo = _.find(existingTodos, {id})
+
+            if (todo) {
+                if ((item.email && todo.email != item.email) ||
+                    (item.title && todo.title != item.title) ||
+                    (item.content && todo.content != item.content)) {
+                    Todos.update({id}, {$set: item})
+                    ids.push(id)
+                }
+            } else {
+
+                ids.push(Todos.insert(item))
+            }
+        })
+
+        return ids
+    },
+
+    removeTodo(id)
+    {
+        Todos.remove({_id: id})
+    },
+});
---
+++ imports/api/models/todos/todos.js
@@ -0,0 +1,64 @@
+import {Mongo} from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+
+class TodosCollection extends Mongo.Collection {
+    insert(doc, callback) {
+        const ourDoc = doc;
+        ourDoc.created_at = ourDoc.created_at || new Date();
+        const result = super.insert(ourDoc, callback);
+        return result
+    }
+
+    remove(selector) {
+        const result = super.remove(selector);
+        return result;
+    }
+}
+
+export const Todos = new TodosCollection("Todos");
+
+// Deny all client-side updates since we will be using methods to manage this collection
+Todos.deny({
+    insert() {
+        return true;
+    },
+    update() {
+        return true;
+    },
+    remove() {
+        return true;
+    }
+});
+
+Todos.schema = new SimpleSchema({
+    _id: {type: String, regEx: SimpleSchema.RegEx.Id},
+    id: {type: String},
+    account_id: {type: String},
+    email: {type: String},
+    title: {type: String, optional: true}
+    content: {type: String, optional: true}
+    //description: {type: String, optional: true},
+    created_at: {type: Date, denyUpdate: true, optional: true},
+    modified_at: {type: Date, denyInsert: true, optional: true}
+});
+
+Todos.attachSchema(Todos.schema);
+
+Todos.publicFields = {
+    id: 1,
+    account_id: 1,
+    email: 1,
+    title: 1,
+    content: 1,
+    //description: 1,
+    created_at: 1,
+    modified_at: 1
+};
+
+Todos.helpers({
+    account() {
+        const {NylasAccounts} = require('../nylasaccounts/nylas-accounts')
+
+        return NylasAccounts.findOne({accountId: this.account_id})
+    }
+});
