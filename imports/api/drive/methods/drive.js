032ab5a16fad63c3488392634723ba2c713c5ec0
Duy Tai Nguyen <duytai.cse@gmail.com>
2017-03-30 18:54:20 +0700

    upload files to project folders

--- imports/api/drive/methods/drive.js
+++ imports/api/drive/methods/drive.js
@@ -7,5 +7,6 @@ const oauth2Client = new OAuth2Client(clientId, clientSecret, redirectUri);
 oauth2Client.setCredentials({
     refresh_token: refreshToken,
 });
+const drive = google.drive({ version: 'v3', auth: oauth2Client });
 
-export default google.drive({ version: 'v3', auth: oauth2Client });
+export { drive, oauth2Client };
--- imports/api/drive/methods/index.js
+++ imports/api/drive/methods/index.js
@@ -2,10 +2,13 @@ import listFiles from './listFiles';
 import createFolder from './createFolder';
 import createSalesRecordFolder from './createSalesRecordFolder';
 import createProjectFolder from './createProjectFolder';
+import getAccessToken from './getAccessToken';
+import getFolderId from './getFolderId';
 
 export {
     listFiles,
     createFolder,
     createSalesRecordFolder,
     createProjectFolder,
+    getFolderId,
 }
