a724b9da1c29f3fe7b0d1c818c3f9143c720fd52
Duy Tai Nguyen <duytai.cse@gmail.com>
2017-04-24 21:22:34 +0700

    Find user with keyword

--- imports/api/tasks/methods/findUsers.js
+++ imports/api/tasks/methods/findUsers.js
@@ -4,11 +4,13 @@ export default new ValidatedMethod({
   name: 'task.findUsers',
   validate: new SimpleSchema({
     keyword: { type: String },
+    ignore: { type: String },
   }).validator(),
-  run({ keyword }) {
+  run({ keyword, ignore }) {
     if (!this.userId) return [];
     const $regex = new RegExp(keyword, 'i');
     return Meteor.users.find({
+      _id: { $ne: ignore },
       roles: 'employee',
       $or: [
         {
