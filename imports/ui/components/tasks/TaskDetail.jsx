c0736dc1d0ed1844bc212cba5d1c4c364711179b
Duy Tai Nguyen <duytai.cse@gmail.com>
2017-04-26 20:02:38 +0700

    Allow edit task name

--- imports/ui/components/tasks/TaskDetail.jsx
+++ imports/ui/components/tasks/TaskDetail.jsx
@@ -3,92 +3,66 @@ import { Modal } from 'react-bootstrap';
 import DatePicker from 'react-datepicker';
 import styled from 'styled-components';
 import moment from 'moment';
-import FindUser from './FindUser.jsx';
+import SelectUser from './SelectUser.jsx';
+import TextEditor from './TextEditor.jsx';
+import TaskName from './TaskName.jsx';
 
 class TaskDetail extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      findUser: {
+      isFinding: {
         assignee: false,
         approver: false,
       },
-      selectedUser: {
+      task: {
+        name: 'Task ###',
         assignee: null,
         approver: null,
+        dueDate: new Date,
+        description: 'This task is about ...',
+        status: props.status,
       },
     };
-    this.toggleFindUser = this.toggleFindUser.bind(this);
+    this.changeState = this.changeState.bind(this);
+    this.saveTask = this.saveTask.bind(this);
   }
 
-  toggleFindUser(prop, propName, propValue) {
+  saveTask() {
+    const parentId = FlowRouter.current().params.id;
+    const { name, assignee, approver, dueDate, description, status } = this.state.task;
+    const task = {
+      name,
+      assignee: assignee ? assignee._id : '',
+      approver: approver ? approver._id : '',
+      description,
+      dueDate,
+      status,
+      parentId,
+    };
+    console.log(task);
+  }
+
+  changeState(prop, propName, propValue) {
     prop[propName] = propValue;
     this.setState(prevState => prevState);
   }
 
   render() {
-    const TaskControl = styled.button `
-      width: 125px;
-    `;
-    const HeaderContainer = styled.div `
-      position: relative;
-    `;
-    const TitleContent = styled.div `
-      position: relative;
-      margin-left: 25px;
-    `;
-    const TitleIcon = styled.div `
-      position: absolute;
-      width: 30px;
-    `;
-    const TextArea = styled.textarea `
-      font-size: 14px;
-      margin-top: 15px;
-      margin-left: 15px;
-      width: 100%;
-      min-height: 100px;
-      border-image: none;
-      border-radius: 6px 6px 6px 6px;
-      border-style: none none none solid;
-      border-width: medium 1px 1px medium;
-      box-shadow: 0 1px 2px rgba(0, 0, 0, 0.12) inset;
-      color: #555555;
-      font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;
-      font-size: 1em;
-      line-height: 1.4em;
-      padding: 5px 8px;
-      transition: background-color 0.2s ease 0s;
-      background: none repeat scroll 0 0 #FFFFFF;
-      border-left-color: green;
-      &:focus {
-        background: none repeat scroll 0 0 rgba(0, 0, 0, 0.07);
-        outline-width: 0;
-      }
-    `;
-    const UserElem = styled.div `
-      color: white;
-      position: relative;
-      background-color: #519839;
-      height: 38px;
-      line-height: 38px;
-      padding-left: 10px;
-      border-radius: 5px;
-      font-weight: 700;
-      overflow-x: hidden;
-      div {
-        position: absolute;
-        top: 0px;
-        right: 20px;
-        background-color: #519839;
-        color: white;
-        border: 0px;
-        outline: none;
-        cursor: pointer;
-      }
-    `;
-    const selectedUser = this.state.selectedUser;
-    const findUser = this.state.findUser;
-
+    const selectUsers = [
+      {
+        name: 'assignee',
+        top: 50,
+        ignore: 'approver',
+        label: 'Assignee',
+      },
+      {
+        name: 'approver',
+        top: 50,
+        ignore: 'assignee',
+        label: 'Approver',
+      },
+    ];
     return (
       <Modal
         show={this.props.isShown}
@@ -98,83 +72,42 @@ class TaskDetail extends Component {
           <div className='row'>
 
             <div className='col-md-9'>
-              <HeaderContainer>
-                <TitleIcon>
-                  <i className='fa fa-credit-card'/>
-                </TitleIcon>
-                <TitleContent>
-                  Convert incoming email to project and attach converstaion
-                </TitleContent>
-              </HeaderContainer>
-              <TextArea
-                defaultValue={'This is a sample'}
+              <TaskName
+                name={this.state.task.name}
+                onChange={name => { this.state.task.name = name }}
+              />
+              <TextEditor
+                content={this.state.task.description}
+                onChange={description => { this.state.task.description = description }}
               />
             </div>
 
             <div className='col-md-3'>
-              <div className='form-group'>
-                {
-                  (selectedUser.assignee) ? (
-                    <UserElem>
-                      { selectedUser.assignee.username }
-                      <div onClick={ ()=> this.toggleFindUser(selectedUser, 'assignee', null) }>
-                        <i className='fa fa-times'/>
-                      </div>
-                    </UserElem>
-                  ) : (
-                    <TaskControl
-                      className='btn btn-default btn-sm'
-                      onClick={()=> this.toggleFindUser(this.state.findUser, 'assignee', !this.state.findUser.assignee)}
-                    >
-                      <i className='fa fa-user-o'> Assignee</i>
-                    </TaskControl>
-                  )
-                }
-                {
-                  (findUser.assignee) ? (
-                    <FindUser
-                      title={'Assignee'}
-                      top={'50px'}
-                      ignore={selectedUser.approver}
-                      selectUser={ (assignee)=> this.toggleFindUser(this.state.selectedUser, 'assignee', assignee) }
-                      close={ ()=> this.toggleFindUser(this.state.findUser, 'assignee', false) }/>
-                  ) : ''
-                }
-              </div>
-              <div className='form-group'>
-                {
-                  (selectedUser.approver) ? (
-                    <UserElem>
-                      { selectedUser.approver.username }
-                      <div onClick={()=> this.toggleFindUser(selectedUser, 'approver', null)}>
-                        <i className='fa fa-times'/>
-                      </div>
-                    </UserElem>
-                  ) : (
-                    <TaskControl
-                      className='btn btn-default btn-sm'
-                      onClick={()=> this.toggleFindUser(this.state.findUser, 'approver', !this.state.findUser.approver)}
-                    >
-                      <i className='fa fa-eye'> Approver</i>
-                    </TaskControl>
-                  )
-                }
-                {
-                  (findUser.approver) ? (
-                    <FindUser
-                      title={'Approver'}
-                      top={'100px'}
-                      ignore={selectedUser.assignee}
-                      selectUser={ (approver)=> this.toggleFindUser(this.state.selectedUser, 'approver', approver) }
-                      close={ ()=> this.toggleFindUser(this.state.findUser, 'approver', false) }/>
-                  ) : ''
-                }
-              </div>
+              {
+                selectUsers.map(({ name, top, label, ignore })=> {
+                  return (
+                    <SelectUser
+                      key={name}
+                      isFinding={this.state.isFinding[name]}
+                      closeFinding={()=> this.changeState(this.state.isFinding, name, false)}
+                      ignoreUser={this.state.task[ignore]}
+                      title={label}
+                      top={top}
+                      user={this.state.task[name]}
+                      onSelectUser={(user)=> this.changeState(this.state.task, name, user)}
+                      removeUser={()=> this.changeState(this.state.task, name, null)}
+                      toggleFinding={()=> this.changeState(this.state.isFinding, name, !this.state.isFinding[name])}
+                    />
+                  );
+                })
+              }
               <div className='form-group'>
                 <label style={{fontSize: '14px'}}>Due Date</label>
                 <div className='form-control'>
                   <DatePicker
-                    selected={moment(new Date())}
+                    selected={moment(this.state.task.dueDate)}
+                    minDate={moment()}
+                    onChange={date => this.changeState(this.state.task, 'dueDate', date.toDate())}
                   />
                 </div>
               </div>
@@ -182,7 +115,7 @@ class TaskDetail extends Component {
           </div>
         </Modal.Body>
         <Modal.Footer>
-          <button className='btn btn-default'>Save</button>
+          <button className='btn btn-default' onClick={ this.saveTask }>Save</button>
         </Modal.Footer>
       </Modal>
     );
@@ -193,6 +126,7 @@ TaskDetail.propTypes = {
   showDetail: PropTypes.func.isRequired,
   hideDetail: PropTypes.func.isRequired,
   isShown: PropTypes.bool.isRequired,
+  status: PropTypes.string.isRequired,
 };
 
 export default TaskDetail;
