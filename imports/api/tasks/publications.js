3887ba40cb1a703f395b95ecabc311a40c803368
Duy Tai Nguyen <duytai.cse@gmail.com>
2017-04-27 14:54:39 +0700

    allow drag and drop task

--- imports/api/tasks/publications.js
+++ imports/api/tasks/publications.js
@@ -4,7 +4,14 @@ Meteor.publishComposite('task.all', function ({ parentId }) {
   if (!Match.test(parentId, String) || !this.userId) return this.ready();
   return {
     find() {
-      return Tasks.find({ parentId, isRemoved: false });
+      return Tasks.find({
+        parentId,
+        isRemoved: false,
+      }, {
+        fields: {
+          isRemoved: 0,
+        },
+      });
     },
 
     children: [
