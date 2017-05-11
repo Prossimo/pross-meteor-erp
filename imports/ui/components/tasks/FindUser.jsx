a944b458d0e282890b7233a3be5bb04e81742455
Duy Tai Nguyen <duytai.cse@gmail.com>
2017-05-11 21:54:46 +0700

    Allow mention other user

--- imports/ui/components/tasks/FindUser.jsx
+++ imports/ui/components/tasks/FindUser.jsx
@@ -15,8 +15,8 @@ class FindUser extends Component {
 
   changeKeyword(keyword) {
     const ignore = this.props.ignore ? this.props.ignore._id : '';
-    Meteor.call('task.findUsers', { keyword, ignore }, (error, users)=> {
-      if (!error) this.setState({ users, keyword });
+    meteor.call('task.findusers', { keyword, ignore }, (error, users)=> {
+      if (!error) this.setstate({ users, keyword });
     });
   }
 
