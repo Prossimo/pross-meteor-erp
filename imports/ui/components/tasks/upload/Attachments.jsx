8b3db934e4495111459108aa5fb0476354382541
Duy Tai Nguyen <duytai.cse@gmail.com>
2017-05-15 20:38:16 +0700

    Drag/drop to add attachments

--- imports/ui/components/tasks/upload/Attachments.jsx
+++ imports/ui/components/tasks/upload/Attachments.jsx
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import moment from 'moment';
 
 class Attachments extends Component {
   constructor() {
@@ -12,52 +13,41 @@ class Attachments extends Component {
           <strong><i className='fa fa-paperclip'/>&nbsp;&nbsp;&nbsp;Attachments</strong>
         </p>
         <div className='attachments'>
-          <div className='attachment'>
-            <div className='attachment-icon'>
-              PDF
-            </div>
-            <div className='attachment-payload'>
-              <div className='attachment-header'>
-                Epidemic
-              </div>
-              <div className='attachment-time'>
-                Added At 14/01/2016
-              </div>
-              <div className='attachment-controls'>
-                <button className='attachment-control'>
-                  <a href='#'><i className='fa fa-download'/> Download</a>
-                </button>
-                <button className='attachment-control'>
-                  <a href='#'><i className='fa fa-times'/> Delete</a>
-                </button>
-              </div>
-            </div>
-          </div>
-          <div className='attachment'>
-            <div className='attachment-icon'>
-              PDF
-            </div>
-            <div className='attachment-payload'>
-              <div className='attachment-header'>
-                Epidemic
-              </div>
-              <div className='attachment-time'>
-                Added At 14/01/2016
-              </div>
-              <div className='attachment-controls'>
-                <button className='attachment-control'>
-                  <a href='#'><i className='fa fa-download'/> Download</a>
-                </button>
-                <button className='attachment-control'>
-                  <a href='#'><i className='fa fa-times'/> Delete</a>
-                </button>
-              </div>
-            </div>
-          </div>
+          {
+            this.props.attachments.map(({ mimeType, name, _id })=> {
+              return (
+                <div className='attachment' key={_id}>
+                  <div className='attachment-icon'>
+                    { _.last(mimeType.split('/')) }
+                  </div>
+                  <div className='attachment-payload'>
+                    <div className='attachment-header'>
+                    { name }
+                    </div>
+                    <div className='attachment-time'>
+                      Added At 14/01/2016
+                    </div>
+                    <div className='attachment-controls'>
+                      <button className='attachment-control'>
+                        <a href='#'><i className='fa fa-download'/> Download</a>
+                      </button>
+                      <button className='attachment-control'>
+                        <a href='#'><i className='fa fa-times'/> Delete</a>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              );
+            })
+          }
         </div>
       </div>
     );
   }
 }
+
+Attachments.propTypes = {
+  attachments: PropTypes.array,
+};
 
 export default Attachments;
