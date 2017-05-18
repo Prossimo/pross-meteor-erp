39e20372411e9733115bd037efc3ba86666b0892
Duy Tai Nguyen <duytai.cse@gmail.com>
2017-05-18 20:45:18 +0700

    update folder id

---
+++ imports/api/settings/index.js
@@ -0,0 +1,10 @@
+import initSettings from './methods/initSettings';
+import updateSetting from './methods/updateSetting';
+import './publications';
+
+initSettings.call({});
+
+export {
+  initSettings,
+  updateSetting,
+};
