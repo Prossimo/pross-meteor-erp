83fb88c13b6f272c7e7395aeeee02f1b855dbf70
Duy Tai Nguyen <duytai.cse@gmail.com>
2017-05-19 17:12:00 +0700

    Change initial settings for project/deal

--- imports/api/settings/methods/initSettings.js
+++ imports/api/settings/methods/initSettings.js
@@ -2,8 +2,8 @@ import SimpleSchema from 'simpl-schema';
 import { Settings } from '../../models/';
 
 const CONFIG = {
-  PROJECT_ROOT_FOLDER: '0B9TUb-58jBJ-WFp3ZmhsVEZqVmc',
-  DEAL_ROOT_FOLDER: '0B2L677Tiv56RbmRDUDdTQnZTbHM',
+  PROJECT_ROOT_FOLDER: '0B9TUb-58jBJ-S25JYUV3c3FNM0E',
+  DEAL_ROOT_FOLDER: '0B9TUb-58jBJ-YzR2Tkwzc1FYVVk',
   PROJECT_TEMPLATE_FOLDER: '0BzKhdC5zjamxY1g4aEtGMGNWY1k',
   DEAL_TEMPLATE_FOLDER: '0BzKhdC5zjamxY1g4aEtGMGNWY1k',
 };
