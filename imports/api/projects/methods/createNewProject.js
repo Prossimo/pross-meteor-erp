7912419a81d5d9965e0451a9b92a2b3d63eb8ebf
Duy Tai Nguyen <duytai.cse@gmail.com>
2017-05-26 14:59:05 +0700

    Create new project with slackchannel

--- imports/api/projects/methods/createNewProject.js
+++ imports/api/projects/methods/createNewProject.js
@@ -1,23 +1,76 @@
+import { HTTP } from 'meteor/http';
+import config from '/imports/api/config/config';
 import { EMPLOYEE_ROLE, ADMIN_ROLE_LIST } from '/imports/api/constants/roles';
 import { Projects } from '/imports/api/models';
 import { prossDocDrive } from '/imports/api/drive';
 
+const { slack: { apiRoot, apiKey } } = config;
+const slackRequest  = ({ action, params }) => {
+  check(action, String);
+  check(params, Object);
+  const { data } = HTTP.post(`${apiRoot}/${action}`, {
+    params: {
+      token: apiKey,
+      ...params,
+    },
+  });
+  return data;
+};
+
 Meteor.methods({
   createNewProject(project) {
-    if (!Roles.userIsInRole(this.userId, [EMPLOYEE_ROLE, ...ADMIN_ROLE_LIST])) {
-      throw new Meteor.Error('Access denied');
-    }
     check(project, {
       name: String,
-      members: [{
-        userId: String,
-        isAdmin: Boolean,
-      }],
+      members: [
+        {
+          userId: String,
+          isAdmin: Boolean,
+        },
+      ],
     });
+
+    // CHECK ROLE
+    if (!Roles.userIsInRole(this.userId, [EMPLOYEE_ROLE, ...ADMIN_ROLE_LIST]))
+      throw new Meteor.Error('Access denied');
+
+    // INSERT
     const projectId = Projects.insert(project);
+
+    // RUN LATER
     Meteor.defer(()=> {
-      prossDocDrive.createProjectFolder.call({ name: project.name, projectId });
+      // CREATE NEW CHANNEL
+      let data = slackRequest({
+        action: 'channels.create',
+        params: { name: project.name },
+      });
+
+      // RETRY WITH UNIQUE NAME
+      if (!data.ok) {
+        data = slackRequest({
+          action: 'channels.create',
+          params: { name: `${project.name}-${Random.id()}` },
+        });
+      }
+
+      if (data.ok) {
+        const slackChannel = data.channel.id;
+
+        // INVITE MEMBERS to CHANNEL
+        Meteor.users.find({
+          _id: { $in: project.members.map(({ userId }) => userId) },
+          slack: { $exists: true },
+        }).forEach(
+          ({ slack: { id } })=> slackRequest({
+            action: 'channels.invite',
+            params: { channel: slackChannel, user: id },
+          })
+        );
+
+        // CREATE DRIVE
+        prossDocDrive.createProjectFolder.call({ name: project.name, projectId });
+      }
+
     });
     return projectId;
-  }
-})
+  },
+});
