06411394880e63107906de94a79fc5a916d80035
Thien Nguyen <hothiennguyen124@gmail.com>
2017-05-28 16:25:53 +0700

    Display deal detail when click on kaban

--- imports/ui/components/salesRecord/kanbanView/KanbanItem.jsx
+++ imports/ui/components/salesRecord/kanbanView/KanbanItem.jsx
@@ -1,27 +1,29 @@
-import React, {Component} from 'react';
-import styled from 'styled-components';
+/* global FlowRouter, moment */
+import React, {Component} from 'react'
+import styled from 'styled-components'
 
 class KanbanItem extends Component {
+
 	render() {
 		const DueDateIcon = styled.div `
-      width: 35px;
-      height: 20px;
-      background-color: #6d6d6d;
-      text-align: center;
-      font-weight: bold;
-      border-radius: 3px;
-      overflow-x: hidden;
-      position: relative;
-      float: right;
-      margin-left: 2px;
-      float: left;
-      width: 80px;
-      color: white;
-    `;
-		const { name, productionStartDate } = this.props.deal
+      width: 35px
+      height: 20px
+      background-color: #6d6d6d
+      text-align: center
+      font-weight: bold
+      border-radius: 3px
+      overflow-x: hidden
+      position: relative
+      float: right
+      margin-left: 2px
+      float: left
+      width: 80px
+      color: white
+    `
+		const { _id, name, createdAt } = this.props.deal
 		return (
 			<div className="task-container"
-				onClick={ this.showDetail }
+				onClick={() => {FlowRouter.go('SalesRecord', {id: _id})} }
 				draggable='true'
 				onDragStart={ event =>
 				 event.dataTransfer.setData('<deal></deal>', JSON.stringify(this.props.deal))
@@ -29,10 +31,10 @@ class KanbanItem extends Component {
 			>
 				<p>{name}</p>
 	      <DueDateIcon>
-          { moment(productionStartDate).format('MM/DD/YYYY') }
+          { moment(createdAt).format('MM/DD/YYYY') }
         </DueDateIcon>
 			</div>
 		)
 	}
 }
-export default KanbanItem;
+export default KanbanItem
