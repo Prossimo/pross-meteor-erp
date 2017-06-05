da2f3c7d638e717c3847739ff2f2c57347356731
Duy Tai Nguyen <duytai.cse@gmail.com>
2017-06-05 08:14:35 +0700

    Limit google access to google drive settings page

--- imports/ui/pages/DriveSettingsPage.jsx
+++ imports/ui/pages/DriveSettingsPage.jsx
@@ -1,61 +1,63 @@
-import React, { Component } from 'react';
-import { createContainer } from 'meteor/react-meteor-data';
-import styled from 'styled-components';
-import picker from 'meteor/picker';
-import swal from 'sweetalert2';
-import Settings from '/imports/api/models/settings/settings';
-import 'sweetalert2/dist/sweetalert2.min.css';
+import React, { Component } from 'react'
+import { createContainer } from 'meteor/react-meteor-data'
+import styled from 'styled-components'
+import picker from 'meteor/picker'
+import swal from 'sweetalert2'
+import { Roles } from 'meteor/alanning:roles'
+import Settings from '/imports/api/models/settings/settings'
+import {ROLES} from '/imports/api/models'
+import 'sweetalert2/dist/sweetalert2.min.css'
+
+const ContentWrapper = styled.div `
+  background-color: white;
+  padding: 20px 30px;
+  min-height: 300px;
+`
 
 class DriveSettingsPage extends Component {
   constructor() {
-    super();
-    picker.then(p => this.picker = p);
-    this.updateFolder = this.updateFolder.bind(this);
-    this.openFolder = this.openFolder.bind(this);
+    super()
+    picker.then(p => this.picker = p)
+    this.updateFolder = this.updateFolder.bind(this)
+    this.openFolder = this.openFolder.bind(this)
   }
 
   componentWillUnMount() {
-    this.props.subs.forEache(sub => sub.stop());
+    this.props.subs.forEach(sub => sub.stop())
   }
 
   updateFolder(key) {
     this.picker.pick(({ docs }) => {
-      const value = docs[0].id;
-      Meteor.call('settings.update', { key, value }, (error, result)=> {
-        if (error) {
-          const msg = error.reason ? error.reason : error.message;
-          swal('Update Settings', msg, 'error');
-        };
-      });
-    });
+      const value = docs[0].id
+      Meteor.call('settings.update', { key, value }, error => {
+        if (!error) return
+        swal('Update Settings', error.reason || error.message, 'error')
+      })
+    })
   }
 
   openFolder(key, value) {
-    Meteor.call('drive.getFiles', { fileId: value }, (error, result)=> {
-      if (error) {
-        const msg = error.reason ? error.reason : error.message;
-        swal('Update Settings', msg, 'error');
-      } else {
-        window.open(result.webViewLink, '_blank');
-      }
-    });
+    Meteor.call('drive.getFiles', { fileId: value }, (error, { webViewLink }) => {
+      error && swal('Update Settings', error.reason || error.message, 'error')
+      !error && window.open(webViewLink, '_blank')
+    })
   }
 
   render() {
-    const ContentWrapper = styled.div `
-      background-color: white;
-      padding: 20px 30px;
-      min-height: 300px;
-    `;
     const folderSettings = this
       .props
       .settings
-      .filter(({ key })=> [
+      .filter(({ key }) => [
         'PROJECT_ROOT_FOLDER',
         'DEAL_ROOT_FOLDER',
         'PROJECT_TEMPLATE_FOLDER',
         'DEAL_TEMPLATE_FOLDER'
-      ].includes(key));
+      ].includes(key))
+
+
+    if (!Roles.userIsInRole(Meteor.userId(), ROLES.ADMIN)) return (
+      <p className='text-center'>Access Denied</p>
+    )
 
     return (
       <ContentWrapper>
@@ -72,39 +74,39 @@ class DriveSettingsPage extends Component {
               </thead>
               <tbody>
                 {
-                  folderSettings.map(({ key, value })=> {
-                    return (
+                  folderSettings.map(({ key, value }) =>
+                    (
                       <tr key={key}>
                         <td>{ key }</td>
                         <td>{ value }</td>
                         <td>
                           <div className='btn-group'>
-                            <button className='btn btn-default btn-sm' onClick={()=> this.updateFolder(key, value)}>
+                            <button className='btn btn-default btn-sm' onClick={() => this.updateFolder(key, value)}>
                               <i className='fa fa-folder-o'/> Update
                             </button>
-                            <button className='btn btn-default btn-sm' onClick={()=> this.openFolder(key, value)}>
+                            <button className='btn btn-default btn-sm' onClick={() => this.openFolder(key, value)}>
                               <i className='fa fa-external-link'/> Open
                             </button>
                           </div>
                         </td>
                       </tr>
-                    );
-                  })
+                    )
+                  )
                 }
               </tbody>
             </table>
           </div>
         </div>
       </ContentWrapper>
-    );
+    )
   }
 }
 
-export default createContainer(()=> {
-  const subs = [];
-  subs.push(Meteor.subscribe('settings.all'));
+export default createContainer(() => {
+  const subs = []
+  subs.push(Meteor.subscribe('settings.all'))
   return {
     subs,
     settings: Settings.find().fetch(),
-  };
-}, DriveSettingsPage);
+  }
+}, DriveSettingsPage)
