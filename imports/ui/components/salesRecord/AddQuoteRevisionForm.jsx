33fc26b83b4aecb524f90cb1d4ffbe1d57716140
Ismail Frolov <i.f.0109@hotmail.com>
2017-06-14 16:07:50 +0300

    Fixed #409

--- imports/ui/components/salesRecord/AddQuoteRevisionForm.jsx
+++ imports/ui/components/salesRecord/AddQuoteRevisionForm.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import Files from '/imports/api/models/files/files';
-import { getUserName, getUserEmail, getAvatarUrl, getSlackUsername } from '../../../api/lib/filters';
-import { generateEmailHtml } from '/imports/api/lib/functions';
-import { warning, info } from '/imports/api/lib/alerts';
-import _ from 'underscore';
-import TemplateSelect from '../mailtemplates/TemplateSelect';
-import TemplateOverview from '../mailtemplates/TemplateOverview';
+import React from 'react'
+import Files from '/imports/api/models/files/files'
+import { getUserName, getUserEmail, getAvatarUrl, getSlackUsername } from '../../../api/lib/filters'
+import { generateEmailHtml } from '/imports/api/lib/functions'
+import { warning, info } from '/imports/api/lib/alerts'
+import _ from 'underscore'
+import TemplateSelect from '../mailtemplates/TemplateSelect'
+import TemplateOverview from '../mailtemplates/TemplateOverview'
 import {NylasUtils, RegExpUtils, Actions, DraftStore} from '/imports/api/nylas'
 import ComposeModal from '../inbox/composer/ComposeModal'
 
@@ -20,9 +20,9 @@ class AddQuoteForm extends React.Component{
     }
 
     constructor(props){
-        super(props);
+        super(props)
 
-        const defaultRevisionNumber = _.isArray(props.quote.revisions) ? props.quote.revisions.length : 0;
+        const defaultRevisionNumber = _.isArray(props.quote.revisions) ? props.quote.revisions.length : 0
 
         this.state = {
             currentFile: null,
@@ -34,8 +34,8 @@ class AddQuoteForm extends React.Component{
 
     changeFileInput = (event) => {
         if(event.target.files.length){
-            if(event.target.files[0].type !== "application/pdf") {
-                return  warning(`You can add only PDF files!`);
+            if(event.target.files[0].type !== 'application/pdf') {
+                return  warning('You can add only PDF files!')
             }
 
             this.setState({
@@ -45,7 +45,7 @@ class AddQuoteForm extends React.Component{
     }
 
     renderAttachedFile(){
-        const { currentFile } = this.state;
+        const { currentFile } = this.state
         if(currentFile){
             return (
                 <div className="attached-file">
@@ -56,89 +56,89 @@ class AddQuoteForm extends React.Component{
     }
 
     revisionNumberChange(event){
-        const { quote } = this.props;
-        const { value: revisionNumber } = event.target;
-        if(revisionNumber > quote.revisions.length) return;
+        const { quote } = this.props
+        const { value: revisionNumber } = event.target
+        if(revisionNumber > quote.revisions.length) return
 
-        this.setState({revisionNumber: parseInt(revisionNumber)});
+        this.setState({revisionNumber: parseInt(revisionNumber)})
     }
 
     formSubmit(event){
-        event.preventDefault();
-        const { currentFile, totalCost, alertsActive, revisionNumber } = this.state;
-        const { salesRecord, usersArr, currentUser, quote } = this.props;
-        if(!currentFile)return warning(`You must add PDF file`);
-        if(totalCost === '')return warning(`Empty total cost field`);
-        if(revisionNumber === '')return warning(`Empty revision number field`);
+        event.preventDefault()
+        const { currentFile, totalCost, alertsActive, revisionNumber } = this.state
+        const { salesRecord, usersArr, currentUser, quote } = this.props
+        if(!currentFile)return warning('You must add PDF file')
+        if(totalCost === '')return warning('Empty total cost field')
+        if(revisionNumber === '')return warning('Empty revision number field')
 
-        const needUpdate = quote.revisions.some(revision=>revision.revisionNumber === revisionNumber);
-        const memberEmails = salesRecord.members.map(member=>getUserEmail(member.user));
+        const needUpdate = quote.revisions.some(revision => revision.revisionNumber === revisionNumber)
+        const memberEmails = salesRecord.members.map(member => getUserEmail(member.user))
         const revisionData = {
             revisionNumber,
             quoteId: quote._id,
             totalPrice: parseFloat(totalCost),
-            [needUpdate?"updateBy":"createBy"]: Meteor.userId(),
-            [needUpdate?"updateAt":"createAt"]: moment().toDate(),
+            [needUpdate?'updateBy':'createBy']: Meteor.userId(),
+            [needUpdate?'updateAt':'createAt']: moment().toDate(),
             fileName: currentFile.name,
             fileId: null
-        };
+        }
 
         const slackMsgParans = {
             username: getSlackUsername(usersArr[Meteor.userId()]),
             icon_url: getAvatarUrl(usersArr[Meteor.userId()]),
             attachments: [
                 {
-                    "color": "#36a64f",
-                    "text": `<${FlowRouter.url(FlowRouter.current().path)}|Go to project ${salesRecord.name}>`
+                    'color': '#36a64f',
+                    'text': `<${FlowRouter.url(FlowRouter.current().path)}|Go to project ${salesRecord.name}>`
                 }
             ]
-        };
-        const slackText = `I just ${needUpdate?"updated":"added"} revision #${revisionNumber}"`;
+        }
+        const slackText = `I just ${needUpdate?'updated':'added'} revision #${revisionNumber}"`
 
-        const file = new FS.File(currentFile);
+        const file = new FS.File(currentFile)
         file.metadata = {
             userId: Meteor.userId(),
             projectId: salesRecord._id,
-        };
+        }
 
 
         const draftClientId = this.props.draftClientId
-        const revisionCb = (err)=>{
+        const revisionCb = (err) => {
             if(this.props.saved) this.props.saved()
-            info(`${needUpdate?"Update":"Add"} revision success!`);
-            if(err) return warning(err.reason);
+            info(`${needUpdate?'Update':'Add'} revision success!`)
+            if(err) return warning(err.reason)
             //// step # 3 - notify slack/email
-            Meteor.call("sendBotMessage", salesRecord.slackChanel, slackText, slackMsgParans);
+            Meteor.call('sendBotMessage', salesRecord.slackChanel, slackText, slackMsgParans)
 
-            if(!alertsActive) return;
+            if(!alertsActive) return
 
             Actions.sendDraft(draftClientId)
-        };
+        }
 
-        const fileInsertCb = (err, res)=>{
-            if (err) return warning(err.reason);
+        const fileInsertCb = (err, res) => {
+            if (err) return warning(err.reason)
             this.setState({
                 currentFile: null,
                 quoteName: ''
-            });
-            info("Success upload file");
-            revisionData.fileId = res._id;
+            })
+            info('Success upload file')
+            revisionData.fileId = res._id
 
             // step # 2 - add new or update revision
             if(needUpdate) {
-                Meteor.call("updateQuoteRevision", revisionData, revisionCb)
+                Meteor.call('updateQuoteRevision', revisionData, revisionCb)
             }else{
-                Meteor.call("addQuoteRevision", revisionData, revisionCb)
+                Meteor.call('addQuoteRevision', revisionData, revisionCb)
             }
-        };
+        }
         // step # 1 - load pdf to FS
-        Files.insert(file, fileInsertCb);
+        Files.insert(file, fileInsertCb)
     }
 
     hide(){
-        const { hide } = this.props;
+        const { hide } = this.props
         if(typeof hide === 'function'){
-            hide();
+            hide()
         }
     }
 
@@ -147,26 +147,28 @@ class AddQuoteForm extends React.Component{
     }
 
     toggleCheck(){
-        const { alertsActive } = this.state;
-        this.setState({alertsActive: !alertsActive});
+        const { alertsActive } = this.state
+        this.setState({alertsActive: !alertsActive})
     }
 
     render() {
-        const { totalCost, alertsActive, revisionNumber, showComposeModal, selectedMailTemplate, shouldBeCompileMailTemplate } = this.state;
-
+        const { totalCost, alertsActive, revisionNumber, showComposeModal, selectedMailTemplate, shouldCompileMailTemplate } = this.state
+        const {draftClientId} = this.props
         let templateData
 
-        if(shouldBeCompileMailTemplate && selectedMailTemplate) {
-            templateData = this.compileTemplate(selectedMailTemplate)
-        } else {
-            const draft = DraftStore.draftForClientId(this.props.draftClientId)
-            templateData = {
-                subject: draft.subject,
-                body: draft.body
+        if(draftClientId) {
+            if(shouldCompileMailTemplate && selectedMailTemplate) {
+                templateData = this.compileTemplate(selectedMailTemplate)
+            } else {
+                const draft = DraftStore.draftForClientId(this.props.draftClientId)
+                templateData = {
+                    subject: draft.subject,
+                    body: draft.body
+                }
             }
         }
 
-        const { quote } = this.props;
+        const { quote } = this.props
         return (
             <div className="add-quote-form">
                 <form className="default-form" onSubmit={this.formSubmit.bind(this)}>
@@ -196,25 +198,31 @@ class AddQuoteForm extends React.Component{
                                onChange={this.changeFileInput}/>
                         {this.renderAttachedFile()}
                     </div>
-                    <input type="checkbox"
-                           id="alert-checkbox"
-                           onChange={this.toggleCheck.bind(this)}
-                           checked={alertsActive}
-                           className="hidden-checkbox"/>
-                    <label htmlFor="alert-checkbox"
-                           className="check-label">Alert stakeholders</label>
-                    {alertsActive && (NylasUtils.hasNylasAccounts() ? <TemplateSelect onChange={this.onSelectMailTemplate} selectedTemplate={selectedMailTemplate}/> : <Alert bsStyle="warning">You need to set inbox to email!</Alert>)}
-                    {alertsActive && selectedMailTemplate && (
-                        <div style={{position:'relative'}}>
-                            <TemplateOverview template={templateData}/>
-                            <i className="fa fa-edit" style={{position:'absolute',top:5,right:5}} onClick={this.onClickEditMail}></i>
-                            <ComposeModal isOpen={showComposeModal}
-                                          clientId={this.props.draftClientId}
-                                          onClose={this.onCloseComposeModal}
-                                          lazySend={true}
-                            />
-                        </div>
-                    )}
+                    {
+                        draftClientId && (
+                            <div>
+                                <input type="checkbox"
+                                       id="alert-checkbox"
+                                       onChange={this.toggleCheck.bind(this)}
+                                       checked={alertsActive}
+                                       className="hidden-checkbox"/>
+                                <label htmlFor="alert-checkbox"
+                                       className="check-label">Alert stakeholders</label>
+                                {alertsActive && (NylasUtils.hasNylasAccounts() ? <TemplateSelect onChange={this.onSelectMailTemplate} selectedTemplate={selectedMailTemplate}/> : <Alert bsStyle="warning">You need to set inbox to email!</Alert>)}
+                                {alertsActive && selectedMailTemplate && (
+                                    <div style={{position:'relative'}}>
+                                        <TemplateOverview template={templateData}/>
+                                        <i className="fa fa-edit" style={{position:'absolute',top:5,right:5}} onClick={this.onClickEditMail}></i>
+                                        <ComposeModal isOpen={showComposeModal}
+                                                      clientId={this.props.draftClientId}
+                                                      onClose={this.onCloseComposeModal}
+                                                      lazySend={true}
+                                        />
+                                    </div>
+                                )}
+                            </div>
+                        )
+                    }
                     <div className="submit-wrap">
                         <button className="btnn primary-btn">Add revision</button>
                     </div>
@@ -226,14 +234,14 @@ class AddQuoteForm extends React.Component{
     onSelectMailTemplate = (template) => {
         this.setState({
             selectedMailTemplate: template,
-            shouldBeCompileMailTemplate: true
+            shouldCompileMailTemplate: true
         })
 
 
     }
 
     compileTemplate = (template) => {
-        if(!template) return
+        if(!template || !this.props.draftClientId) return
 
         const tplData = {
             project: this.props.salesRecord.name,
@@ -263,9 +271,9 @@ class AddQuoteForm extends React.Component{
         const draft = DraftStore.draftForClientId(this.props.draftClientId)
 
         this.setState({
-            shouldBeCompileMailTemplate: false
+            shouldCompileMailTemplate: false
         })
     }
 }
 
-export default AddQuoteForm;
+export default AddQuoteForm
