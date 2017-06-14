f51214bc1d9138303b3cd77dc4c6062a34c5eafa
Ismail Frolov <i.f.0109@hotmail.com>
2017-06-14 12:45:43 +0300

    Fixed #419 - add designation and role requirement for people

--- imports/api/lib/alerts.js
+++ imports/api/lib/alerts.js
@@ -1,19 +1,19 @@
-import Alert from 'react-s-alert';
+import Alert from 'react-s-alert'
 
-export const warning = (text)=>{
+export const warning = (text) => {
     if(text)
     return Alert.warning(text, {
         position: 'bottom-right',
         effect: 'bouncyflip',
         timeout: 5000
-    });
+    })
 }
 
-export const info = (text)=>{
+export const info = (text) => {
     if(text)
         return Alert.info(text, {
             position: 'bottom-right',
             effect: 'bouncyflip',
             timeout: 3500
-        });
+        })
 }
