f176d9e5a79de24edcbd99b1983cb4b110f6e9ba
Duy Tai Nguyen <duytai.cse@gmail.com>
2017-06-15 14:33:56 +0700

    Remove ### from foldername

--- imports/api/drive/methods/createSalesRecordFolder.js
+++ imports/api/drive/methods/createSalesRecordFolder.js
@@ -1,9 +1,10 @@
-import SimpleSchema from 'simpl-schema';
-import { SalesRecords, Settings } from '/imports/api/models';
-import listFiles from './listFiles';
-import copyFiles from './copyFiles';
-import createFolder from './createFolder';
-import shareWith from './shareWith';
+import SimpleSchema from 'simpl-schema'
+import { SalesRecords, Settings } from '/imports/api/models'
+import listFiles from './listFiles'
+import copyFiles from './copyFiles'
+import createFolder from './createFolder'
+import shareWith from './shareWith'
+import { ValidatedMethod } from 'meteor/mdg:validated-method'
 
 export default new ValidatedMethod({
   name: 'drive.createSalesRecordFolder',
@@ -12,36 +13,34 @@ export default new ValidatedMethod({
     salesRecordId: { type: String },
   }).validator(),
   run({ name, salesRecordId }) {
-    const { value: salesRecordParentFolderId } = Settings.findOne({ key: 'DEAL_ROOT_FOLDER' });
-    const { value: dealTempletFolder } = Settings.findOne({ key: 'DEAL_TEMPLATE_FOLDER' });
-    const salesRecordName = `##### ${name}`;
-    const taskName = `Tasks`;
-    const { id: folderId } = createFolder.call({ name: salesRecordName, parent: salesRecordParentFolderId });
-    const { id: taskFolderId } = createFolder.call({ name: taskName, parent: folderId });
+    const { value: salesRecordParentFolderId } = Settings.findOne({ key: 'DEAL_ROOT_FOLDER' })
+    const { value: dealTempletFolder } = Settings.findOne({ key: 'DEAL_TEMPLATE_FOLDER' })
+    const { id: folderId } = createFolder.call({ name, parent: salesRecordParentFolderId })
+    const { id: taskFolderId } = createFolder.call({ name: 'Tasks', parent: folderId })
     SalesRecords.update(salesRecordId, {
       $set: {
         folderId,
         taskFolderId,
       },
-    });
+    })
 
     // copy template to new created folder
-    const { files } = listFiles.call({ query: `'${dealTempletFolder}' in parents` });
-    files.forEach((file)=> {
-      copyFiles.call({ fileId: file.id, parentId: folderId });
-    });
+    const { files } = listFiles.call({ query: `'${dealTempletFolder}' in parents` })
+    files.forEach(file => {
+      copyFiles.call({ fileId: file.id, parentId: folderId })
+    })
 
     // share folder with members
-    const salesRecord = SalesRecords.findOne(salesRecordId);
+    const salesRecord = SalesRecords.findOne(salesRecordId)
     if (salesRecord) {
-      const memberIds = salesRecord.members.map(({ userId })=> userId);
+      const memberIds = salesRecord.members.map(({ userId }) => userId)
       const emails = Meteor
         .users
         .find({ _id: { $in: memberIds } })
         .fetch()
-        .filter(({ emails })=> emails && emails.length > 0)
-        .map(({ emails })=> emails[0].address);
-      emails.forEach(email => shareWith.call({ fileId: folderId, email }));
+        .filter(({ emails }) => emails && emails.length > 0)
+        .map(({ emails }) => emails[0].address)
+      emails.forEach(email => shareWith.call({ fileId: folderId, email }))
     }
   },
-});
+})
