8084232eedcd6379f19f9c13e2e072749213caeb
Ismail Frolov <i.f.0109@hotmail.com>
2017-06-25 18:29:38 +0300

    Add isDefault property to template

--- imports/ui/components/mailtemplates/TemplatesList.js
+++ imports/ui/components/mailtemplates/TemplatesList.js
@@ -1,24 +1,22 @@
 import React from 'react'
+import TrackerReact from 'meteor/ultimatejs:tracker-react'
 import {Button, Table, InputGroup, FormControl} from 'react-bootstrap'
 import MailTemplates from '/imports/api/models/mailtemplates/mailtemplates'
 
 
-export default class TemplatesList extends React.Component {
+export default class TemplatesList extends TrackerReact(React.Component) {
     static propTypes = {
         onSelectTemplate: React.PropTypes.func,
         onCreateTemplate: React.PropTypes.func,
-        selectedTemplate: React.PropTypes.object,
-        reloadData: React.PropTypes.bool
+        selectedTemplate: React.PropTypes.object
     }
 
     constructor(props) {
         super(props)
 
-        const templates = MailTemplates.find().fetch()
-        const selectedTemplate = props.selectedTemplate || templates[0]
+        const selectedTemplate = props.selectedTemplate
         this.state = {
-            templates: templates,
-            selectedTemplate: selectedTemplate
+            selectedTemplate
         }
 
     }
@@ -32,23 +30,18 @@ export default class TemplatesList extends React.Component {
     }
 
     componentWillReceiveProps(nextProps) {
-        if(nextProps.reloadData) {
+        const {selectedTemplate} = nextProps
 
-            this.setState({
-                templates: MailTemplates.find().fetch(),
-                selectedTemplate: nextProps.selectedTemplate
-            })
+        if(selectedTemplate) {
+            this.setState({selectedTemplate})
         }
-
-
-
     }
 
     render() {
 
         return (
             <div className="templates-list">
-                {this.renderToolbar()}
+                {Meteor.user().isAdmin() && this.renderToolbar()}
 
                 {this.renderContent()}
             </div>
@@ -75,6 +68,7 @@ export default class TemplatesList extends React.Component {
                     <tr>
                         <th>#</th>
                         <th>Subject</th>
+                        <th>Default</th>
                     </tr>
                     </thead>
                     <tbody>
@@ -86,19 +80,27 @@ export default class TemplatesList extends React.Component {
     }
 
     renderRows() {
-        let {templates, selectedTemplate} = this.state
-
+        const templates = MailTemplates.find().fetch()
         if (!templates || templates.length == 0) return ''
+
+        const {selectedTemplate} = this.state
 
         return templates.map((template, index) => (
             <tr className={selectedTemplate && selectedTemplate._id === template._id ? 'focused' : ''} key={template._id}
                 onClick={() => this.onSelectTemplate(template)}>
                 <td>{index + 1}</td>
                 <td>{template.subject}</td>
+                <td><input type="radio" checked={template.isDefault} onChange={() => this.setTemplateAsDefault(template)}/> </td>
             </tr>
         ))
     }
 
+    setTemplateAsDefault = (template) => {
+        if(!Meteor.user().isAdmin()) return
+        Meteor.call('updateTemplate', template._id, {isDefault:true}, (err, res) => {
+            this.onSelectTemplate(res)
+        })
+    }
     onSelectTemplate = (template) => {
         this.setState({selectedTemplate: template})
         if (this.props.onSelectTemplate) this.props.onSelectTemplate(template)
