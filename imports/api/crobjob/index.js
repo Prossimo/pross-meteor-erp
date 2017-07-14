e83a014f28e8f1db4e77ca14549e422fc2bc814e
Duy Tai Nguyen <duytai.cse@gmail.com>
2017-07-14 08:52:05 +0700

    send slack message every 5 minutes

--- imports/api/crobjob/index.js
+++ imports/api/crobjob/index.js
@@ -1,10 +1,17 @@
 import { SyncedCron } from 'meteor/percolate:synced-cron'
 import backupEmailAttachments from './backupEmailAttachments'
+import { observeGoogleFile, registerCallback } from './editGoogleFile'
 
 SyncedCron.add({
-  name: 'backup email attachments',
-  schedule: parser => parser.text('every 30 minutes'),
-  job: backupEmailAttachments,
+  name: 'observe google file',
+  schedule: parser => parser.text('every 5 minutes'),
+  job: observeGoogleFile,
 })
 
-//SyncedCron.start()
+SyncedCron.add({
+  name: 'register callback',
+  schedule: parser => parser.text('every 1 hours'),
+  job: registerCallback,
+})
+
+SyncedCron.start()
