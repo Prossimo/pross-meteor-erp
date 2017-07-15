1a60f4d78236d76956619860b14c0773489b6020
Duy Tai Nguyen <duytai.cse@gmail.com>
2017-07-15 10:28:25 +0700

    disable google notification

--- imports/api/crobjob/index.js
+++ imports/api/crobjob/index.js
@@ -2,16 +2,16 @@ import { SyncedCron } from 'meteor/percolate:synced-cron'
 import backupEmailAttachments from './backupEmailAttachments'
 import { observeGoogleFile, registerCallback } from './editGoogleFile'
 
-SyncedCron.add({
-  name: 'observe google file',
-  schedule: parser => parser.text('every 5 minutes'),
-  job: observeGoogleFile,
-})
+//SyncedCron.add({
+  //name: 'observe google file',
+  //schedule: parser => parser.text('every 5 minutes'),
+  //job: observeGoogleFile,
+//})
 
-SyncedCron.add({
-  name: 'register callback',
-  schedule: parser => parser.text('every 1 hours'),
-  job: registerCallback,
-})
+//SyncedCron.add({
+  //name: 'register callback',
+  //schedule: parser => parser.text('every 1 hours'),
+  //job: registerCallback,
+//})
 
 SyncedCron.start()
