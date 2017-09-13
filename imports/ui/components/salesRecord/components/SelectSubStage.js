c090db85b3c24c321b2fef46e810d5aded4320b3
Ismail Frolov <i.f.0109@hotmail.com>
2017-09-13 14:53:06 +0300

    Fix #615

--- imports/ui/components/salesRecord/components/SelectSubStage.js
+++ imports/ui/components/salesRecord/components/SelectSubStage.js
@@ -1,69 +1,82 @@
 import React, {Component} from 'react'
 import Select from 'react-select'
 import {
-  SUB_STAGES_LEAD,
-  SUB_STAGES_OPP,
-  SUB_STAGES_ORDER,
-  SUB_STAGE_TICKET
+    SUB_STAGES_LEAD,
+    SUB_STAGES_OPP,
+    SUB_STAGES_ORDER,
+    SUB_STAGE_TICKET
 } from '../../../../api/constants/project'
 
 class SelectSubStage extends Component {
-  constructor() {
-    super()
-    this.getSubStages = this.getSubStages.bind(this)
-    this.changeSelectionValue = this.changeSelectionValue.bind(this)
-    this.state = {
-      subStage: ''
+    constructor() {
+        super()
+        this.getSubStages = this.getSubStages.bind(this)
+        this.changeSelectionValue = this.changeSelectionValue.bind(this)
+        this.state = {
+            subStage: ''
+        }
     }
-  }
-  componentWillMount() {
-    const {update, subStage} = this.props
-    if (update) {
-      this.setState({subStage: subStage})
-    } else {
-      const stages = this.getSubStages()
-      if (!_.isEmpty(stages)) {
-        this.setState({subStage: stages[0]})
-        this.props.onSelectSubStage(stages[0])
-      }
+
+    componentWillMount() {
+        const {update, subStage} = this.props
+        if (update) {
+            this.setState({subStage})
+        } else {
+            const stages = this.getSubStages()
+            if (!_.isEmpty(stages)) {
+                this.setState({subStage: stages[0]})
+                this.props.onSelectSubStage(stages[0])
+            }
+        }
     }
-  }
-  componentWillReceiveProps(props) {
-    if (this.props.stage !== props.stage) {
-      if (props.update) {
-        this.setState({subStage: props.subStage})
-      } else {
-        const stages = this.getSubStages(props.stage)
-        this.setState({subStage: stages[0]})
-      }
+
+    componentWillReceiveProps(props) {
+        if (this.props.stage !== props.stage) {
+            let subStage
+            if (props.update) {
+                subStage = props.subStage
+            } else {
+                const stages = this.getSubStages(props.stage)
+                subStage = stages[0]
+            }
+            this.setState({subStage})
+            if (this.props.onSelectSubStage) this.props.onSelectSubStage(subStage)
+        }
     }
-  }
-  getSubStages(stage) {
-    if (!stage) stage = this.props.stage
-    switch (stage) {
-      case 'lead': return SUB_STAGES_LEAD
-      case 'opportunity': return SUB_STAGES_OPP
-      case 'order': return SUB_STAGES_ORDER
-      case 'ticket': return SUB_STAGE_TICKET
-      default: return []
+
+    getSubStages(stage) {
+        if (!stage) stage = this.props.stage
+        switch (stage) {
+            case 'lead':
+                return SUB_STAGES_LEAD
+            case 'opportunity':
+                return SUB_STAGES_OPP
+            case 'order':
+                return SUB_STAGES_ORDER
+            case 'ticket':
+                return SUB_STAGE_TICKET
+            default:
+                return []
+        }
     }
-  }
-  changeSelectionValue(value) {
-    this.setState({subStage: value})
-    this.props.onSelectSubStage(value)
-  }
-  render() {
-    const { style = '', update = false } = this.props
-    return (
-        <Select
-          className={style}
-          value={this.state.subStage}
-          onChange={this.changeSelectionValue}
-          options={this.getSubStages()}
-          clearable={false}
-        />
-    )
-  }
+
+    changeSelectionValue(value) {
+        this.setState({subStage: value})
+        this.props.onSelectSubStage(value)
+    }
+
+    render() {
+        const {style = '', update = false} = this.props
+        return (
+            <Select
+                className={style}
+                value={this.state.subStage}
+                onChange={this.changeSelectionValue}
+                options={this.getSubStages()}
+                clearable={false}
+            />
+        )
+    }
 }
 
 export default SelectSubStage
