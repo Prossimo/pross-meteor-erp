e74ed4fab22f8b2f1bfc4cec62859e0e2a6d2c5e
Ismail Frolov <i.f.0109@hotmail.com>
2017-09-18 17:55:36 +0300

    Fix adding project issue

--- imports/api/nylas/tasks/change-folder-task.js
+++ imports/api/nylas/tasks/change-folder-task.js
@@ -1,6 +1,6 @@
-import _ from 'underscore';
-import ChangeMailTask from './change-mail-task';
-import SyncbackCategoryTask from './syncback-category-task';
+import _ from 'underscore'
+import ChangeMailTask from './change-mail-task'
+import SyncbackCategoryTask from './syncback-category-task'
 
 // Public: Create a new task to apply labels to a message or thread.
 //
@@ -16,67 +16,67 @@ import SyncbackCategoryTask from './syncback-category-task';
 export default class ChangeFolderTask extends ChangeMailTask {
 
     constructor(options = {}) {
-        super(options);
-        this.taskDescription = options.taskDescription;
-        this.folder = options.folder;
+        super(options)
+        this.taskDescription = options.taskDescription
+        this.folder = options.folder
     }
 
     label() {
         if (this.folder) {
-            return `Moving to ${this.folder.display_name}…`;
+            return `Moving to ${this.folder.display_name}…`
         }
-        return "Moving to folder…";
+        return 'Moving to folder…'
     }
 
     categoriesToAdd() {
-        return [this.folder];
+        return [this.folder]
     }
 
     description() {
         if (this.taskDescription) {
-            return this.taskDescription;
+            return this.taskDescription
         }
 
-        let folderText = "";
+        let folderText = ''
         //if (this.folder instanceof Category) {
-            folderText = ` to ${this.folder.display_name}`;
+            folderText = ` to ${this.folder.display_name}`
         //}
 
         if (this.threads.length > 0) {
             if (this.threads.length > 1) {
-                return `Moved ${this.threads.length} threads${folderText}`;
+                return `Moved ${this.threads.length} threads${folderText}`
             }
-            return `Moved 1 thread${folderText}`;
+            return `Moved 1 thread${folderText}`
         }
         if (this.messages.length > 0) {
             if (this.messages.length > 1) {
-                return `Moved ${this.messages.length} messages${folderText}`;
+                return `Moved ${this.messages.length} messages${folderText}`
             }
-            return `Moved 1 message${folderText}`;
+            return `Moved 1 message${folderText}`
         }
-        return `Moved objects${folderText}`;
+        return `Moved objects${folderText}`
     }
 
     isDependentOnTask(other) {
-        return (other instanceof SyncbackCategoryTask);
+        return (other instanceof SyncbackCategoryTask)
     }
 
     performLocal() {
         if (!this.folder) {
-            return Promise.reject(new Error("Must specify a `folder`"))
+            return Promise.reject(new Error('Must specify a `folder`'))
         }
         if (this.threads.length > 0 && this.messages.length > 0) {
-            return Promise.reject(new Error("ChangeFolderTask: You can move `threads` or `messages` but not both"))
+            return Promise.reject(new Error('ChangeFolderTask: You can move `threads` or `messages` but not both'))
         }
         if (this.threads.length === 0 && this.messages.length === 0) {
-            return Promise.reject(new Error("ChangeFolderTask: You must provide a `threads` or `messages` Array of models or IDs."))
+            return Promise.reject(new Error('ChangeFolderTask: You must provide a `threads` or `messages` Array of models or IDs.'))
         }
 
-        return super.performLocal();
+        return super.performLocal()
     }
 
     processNestedMessages() {
-        return false;
+        return false
     }
 
     changesToModel(model) {
@@ -86,15 +86,15 @@ export default class ChangeFolderTask extends ChangeMailTask {
         if (model.object == 'message') {
             return {folders: [this.folder]}
         }
-        return null;
+        return null
     }
 
     requestBodyForModel(model) {
         if (model.object == 'thread') {
-            return {folder: model.folders[0] ? model.folders[0].id : null};
+            return {folder: model.folders[0] ? model.folders[0].id : null}
         }
         if (model.object == 'message') {
-            return {folder: model.folder ? model.folder.id : null};
+            return {folder: model.folder ? model.folder.id : null}
         }
     }
 }
