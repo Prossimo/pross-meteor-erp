4e2e8805192d9be1cbf9ec8d8e9e8efde5874c9e
Ismail Frolov <i.f.0109@hotmail.com>
2017-11-10 10:25:49 +0200

    Fix update slackchannelname bug

--- imports/api/slack/hooks/onChangeProjectName.js
+++ imports/api/slack/hooks/onChangeProjectName.js
@@ -1,28 +1,36 @@
-import { Projects } from '/imports/api/models'
+import {Projects} from '/imports/api/models'
 import slackClient from '../restful'
-import { Random } from 'meteor/random'
-import { Meteor } from 'meteor/meteor'
+import {Random} from 'meteor/random'
+import {Meteor} from 'meteor/meteor'
 
 Projects.before.update((userId, doc, fieldNames, modifier) => {
-  if (fieldNames.includes('name')) {
-    if (modifier.$set && modifier.$set.name) {
-      const { name, slackChanel } = doc
-      const newName = `p-${modifier.$set.name}`
-      if (newName !== name) {
-        Meteor.defer(() => {
-          const { data: { ok } } = slackClient.channels.rename({
-            name: newName,
-            channel: slackChanel,
-          })
-          if (!ok) {
-            slackClient.channels.rename({
-              name: `${newName}-${Random.id()}`,
-              channel: slackChanel,
-            })
-          }
-        })
-      }
+    if (fieldNames.includes('name')) {
+        if (modifier.$set && modifier.$set.name) {
+            const {name, slackChanel} = doc
+            let newName = `p-${modifier.$set.name}`
+            if (newName !== name) {
+                Meteor.defer(() => {
+                    const updateSlackChannelName = (slackChannelName) => {
+                        Projects.update(doc._id, {$set:{slackChannelName}})
+                    }
+                    const {data: {ok}} = slackClient.channels.rename({
+                        name: newName,
+                        channel: slackChanel,
+                    })
+                    if (!ok) {
+                        newName = `${newName}-${Random.id()}`
+                        const {data: {ok}} = slackClient.channels.rename({
+                            name: newName,
+                            channel: slackChanel,
+                        })
+
+                        if(ok) updateSlackChannelName(newName)
+                    } else {
+                        updateSlackChannelName(newName)
+                    }
+                })
+            }
+        }
     }
-  }
 })
 
--- imports/api/slack/hooks/onChangeSalesRecord.js
+++ imports/api/slack/hooks/onChangeSalesRecord.js
@@ -1,27 +1,35 @@
-import { SalesRecords } from '/imports/api/models'
+import {SalesRecords} from '/imports/api/models'
 import slackClient from '../restful'
-import { Random } from 'meteor/random'
-import { Meteor } from 'meteor/meteor'
+import {Random} from 'meteor/random'
+import {Meteor} from 'meteor/meteor'
 
 SalesRecords.before.update((userId, doc, fieldNames, modifier) => {
-  if (fieldNames.includes('name')) {
-    if (modifier.$set && modifier.$set.name) {
-      const { name, slackChanel } = doc
-      const newName = `d-${modifier.$set.name}`
-      if (newName !== name) {
-        Meteor.defer(() => {
-          const { data: { ok } } = slackClient.channels.rename({
-            name: newName,
-            channel: slackChanel,
-          })
-          if (!ok) {
-            slackClient.channels.rename({
-              name: `${newName}-${Random.id()}`,
-              channel: slackChanel,
-            })
-          }
-        })
-      }
+    if (fieldNames.includes('name')) {
+        if (modifier.$set && modifier.$set.name) {
+            const {name, slackChanel} = doc
+            let newName = `d-${modifier.$set.name}`
+            if (newName !== name) {
+                Meteor.defer(() => {
+                    const updateSlackChannelName = (slackChannelName) => {
+                        SalesRecords.update(doc._id, {$set: {slackChannelName}})
+                    }
+                    const {data: {ok}} = slackClient.channels.rename({
+                        name: newName,
+                        channel: slackChanel,
+                    })
+                    if (!ok) {
+                        newName = `${newName}-${Random.id()}`
+                        const {data: {ok}} = slackClient.channels.rename({
+                            name: newName,
+                            channel: slackChanel,
+                        })
+
+                        if (ok) updateSlackChannelName(newName)
+                    } else {
+                        updateSlackChannelName(newName)
+                    }
+                })
+            }
+        }
     }
-  }
 })
