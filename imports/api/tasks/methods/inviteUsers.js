96e5db4d9f32f98820ce454ce813b2990860859f
Ismail Frolov <i.f.0109@hotmail.com>
2017-11-10 09:40:20 +0200

    Fix slack invitation on task creating/updating

--- imports/api/tasks/methods/inviteUsers.js
+++ imports/api/tasks/methods/inviteUsers.js
@@ -1,61 +1,74 @@
-import { ValidatedMethod } from 'meteor/mdg:validated-method'
+import {ValidatedMethod} from 'meteor/mdg:validated-method'
 import SimpleSchema from 'simpl-schema'
-import { Projects, SalesRecords } from '../../models'
+import {Projects, SalesRecords} from '../../models'
+import {slackClient} from '/imports/api/slack'
 
 export default new ValidatedMethod({
-  name: 'task.inviteUsers',
-  validate: new SimpleSchema({
-    parentId: {
-      type: String,
+    name: 'task.inviteUsers',
+    validate: new SimpleSchema({
+        parentId: {
+            type: String,
+        },
+        taskOperators: {
+            type: Array,
+        },
+        'taskOperators.$': {
+            type: String,
+        },
+    }).validator(),
+    run({parentId, taskOperators}) {
+        /*
+        * Add employee and approver to current project
+        * */
+        const project = Projects.findOne(parentId)
+        const salesRecord = SalesRecords.findOne(parentId)
+        const taskParent = project || salesRecord
+        if (taskParent) {
+            const {members = []} = taskParent
+            const memberIds = project ? members.filter(m => m && m.userId).map(({userId}) => userId) : members
+
+            // Invite assigned users to project/saleRecord
+            let willInviteUserIds = taskOperators
+                .filter(userId => !memberIds.includes(userId) && !!userId)
+                .map(userId => {
+                    if (project) {
+                        return {userId, isAdmin: false, category: []}
+                    } else {
+                        return userId
+                    }
+                })
+
+            // Invite user to project
+            if (project) {
+                Projects.update(parentId, {
+                    $push: {
+                        members: {
+                            $each: willInviteUserIds,
+                        },
+                    },
+                })
+            }
+
+            // Invite user to saleRecord
+            if (salesRecord) {
+                SalesRecords.update(parentId, {
+                    $push: {
+                        members: {
+                            $each: willInviteUserIds,
+                        },
+                    },
+                })
+            }
+
+            willInviteUserIds = project ? willInviteUserIds.map(({userId}) => (userId)) : willInviteUserIds
+
+            Meteor.users.find({_id: {$in: willInviteUserIds}, slack: {$exists: true}})
+                .forEach(user => {
+                    slackClient.channels.invite({
+                        channel: taskParent.slackChanel,
+                        user: user.slack.id,
+                    })
+                })
+        }
     },
-    taskOperators: {
-      type: Array,
-    },
-    'taskOperators.$': {
-      type: String,
-    },
-  }).validator(),
-  run({ parentId, taskOperators }) {
-    /*
-    * Add employee and approver to current project
-    * */
-    const project = Projects.findOne(parentId)
-    const salesRecord = SalesRecords.findOne(parentId)
-    const taskParent = project || salesRecord
-    if (taskParent) {
-      const { members = [] } = taskParent
-      const memberIds = members.filter(m => m && m.userId).map(({ userId }) => userId)
-
-      // Invite assigned users to project/saleRecord
-      const willInviteUserIds = taskOperators
-        .filter(userId => !memberIds.includes(userId) && !!userId)
-        .map(userId => ({ userId, isAdmin: false, category: [] }))
-
-      // Invite user to project
-      if (project) {
-        Projects.update(parentId, {
-          $push: {
-            members: {
-              $each: willInviteUserIds,
-            },
-          },
-        })
-      };
-
-      // Invite user to saleRecord
-      if (salesRecord) {
-        SalesRecords.update(parentId, {
-          $push: {
-            members: {
-              $each: willInviteUserIds,
-            },
-          },
-        })
-      }
-
-      /*
-       * TODO: Invite to slack channel
-       * */
-    };
-  },
 })
