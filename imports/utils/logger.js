6aa3b561445e57840cd3638acdf76ee7768629c4
Ismail Frolov <i.f.0109@hotmail.com>
2017-11-14 14:14:15 +0200

    Update custom logger

--- imports/utils/logger.js
+++ imports/utils/logger.js
@@ -8,59 +8,59 @@ if (Meteor.isServer) {
     if (!fs.existsSync(logPath)) fs.mkdirSync(logPath)
 }
 
-    console.log(`====> Logs path: ${Meteor.absolutePath}/logs`)
-    export const ErrorLog = new Logger();
-    (new LoggerFile(ErrorLog, {
-        fileNameFormat(time) {
-            return `mavrik-error-${  time.getDate()  }-${  time.getMonth() + 1  }-${  time.getFullYear()  }  }.log`
-        },
-        format(time, level, message, data, userId) {
-            data = JSON.parse(data)
-            let logString = `[${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}] | "${message}" | User: ${  userId  }\r\n`
-            if (data && Object.keys(data).length > 0) logString += `${JSON.stringify(data)}\r\n`
-            return logString
-        },
-        path: logPath
-    })).enable()
+console.log(`====> Logs path: ${Meteor.absolutePath}/logs`)
+export const ErrorLog = new Logger();
+(new LoggerFile(ErrorLog, {
+    fileNameFormat(time) {
+        return `mavrik-error-${  time.getDate()  }-${  time.getMonth() + 1  }-${  time.getFullYear()  }  .log`
+    },
+    format(time, level, message, data, userId) {
+        data = JSON.parse(data)
+        let logString = `[${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}] | "${message}" | User: ${  userId  }\r\n`
+        if (data && Object.keys(data).length > 0) logString += `${JSON.stringify(data)}\r\n`
+        return logString
+    },
+    path: logPath
+})).enable()
 
-    export const ServerErrorLog = new Logger();
-    (new LoggerFile(ServerErrorLog, {
-        fileNameFormat(time) {
-            return `mavrik-server-error-${  time.getDate()  }-${  time.getMonth() + 1  }-${  time.getFullYear()  }  }.log`
-        },
-        format(time, level, message, data, userId) {
-            data = JSON.parse(data)
-            let logString = `[${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}] | "${message}" | User: ${  userId  }\r\n`
-            if (data && Object.keys(data).length > 0) logString += `${JSON.stringify(data)}\r\n`
-            return logString
-        },
-        path: logPath
-    })).enable()
+export const ServerErrorLog = new Logger();
+(new LoggerFile(ServerErrorLog, {
+    fileNameFormat(time) {
+        return `mavrik-server-error-${  time.getDate()  }-${  time.getMonth() + 1  }-${  time.getFullYear()  }  .log`
+    },
+    format(time, level, message, data, userId) {
+        data = JSON.parse(data)
+        let logString = `[${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}] | "${message}" | User: ${  userId  }\r\n`
+        if (data && Object.keys(data).length > 0) logString += `${JSON.stringify(data)}\r\n`
+        return logString
+    },
+    path: logPath
+})).enable()
 
-    export const MeteorErrorLog = new Logger();
-    (new LoggerFile(MeteorErrorLog, {
-        fileNameFormat(time) {
-            return `mavrik-meteor-error-${  time.getDate()  }-${  time.getMonth() + 1  }-${  time.getFullYear()  }  }.log`
-        },
-        format(time, level, message, data, userId) {
-            data = JSON.parse(data)
-            let logString = `[${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}] | "${message}" | User: ${  userId  }\r\n`
-            if (data && Object.keys(data).length > 0) logString += `${JSON.stringify(data)}\r\n`
-            return logString
-        },
-        path: logPath
-    })).enable()
+export const MeteorErrorLog = new Logger();
+(new LoggerFile(MeteorErrorLog, {
+    fileNameFormat(time) {
+        return `mavrik-meteor-error-${  time.getDate()  }-${  time.getMonth() + 1  }-${  time.getFullYear()  }  .log`
+    },
+    format(time, level, message, data, userId) {
+        data = JSON.parse(data)
+        let logString = `[${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}] | "${message}" | User: ${  userId  }\r\n`
+        if (data && Object.keys(data).length > 0) logString += `${JSON.stringify(data)}\r\n`
+        return logString
+    },
+    path: logPath
+})).enable()
 
-    export const ClientErrorLog = new Logger();
-    (new LoggerFile(ClientErrorLog, {
-        fileNameFormat(time) {
-            return `mavrik-client-error-${  time.getDate()  }-${  time.getMonth() + 1  }-${  time.getFullYear()  }  }.log`
-        },
-        format(time, level, message, data, userId) {
-            data = JSON.parse(data)
-            let logString = `[${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}] | "${message}" | User: ${  userId  }\r\n`
-            if (data && Object.keys(data).length > 0) logString += `${JSON.stringify(data)}\r\n`
-            return logString
-        },
-        path: logPath
-    })).enable()
+export const ClientErrorLog = new Logger();
+(new LoggerFile(ClientErrorLog, {
+    fileNameFormat(time) {
+        return `mavrik-client-error-${  time.getDate()  }-${  time.getMonth() + 1  }-${  time.getFullYear()  }  .log`
+    },
+    format(time, level, message, data, userId) {
+        data = JSON.parse(data)
+        let logString = `[${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}] | "${message}" | User: ${  userId  }\r\n`
+        if (data && Object.keys(data).length > 0) logString += `${JSON.stringify(data)}\r\n`
+        return logString
+    },
+    path: logPath
+})).enable()
