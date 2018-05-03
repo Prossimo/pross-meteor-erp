788a7659ecadace70af0808492a6392a0bc1f33a
Ismail Frolov <i.f.0109@hotmail.com>
2018-05-03 11:34:05 +0300

    Rollback mup-production

--- mup-production.js
+++ mup-production.js
@@ -24,7 +24,7 @@ module.exports = {
 
         env: {
             ROOT_URL: 'https://crm.mavrik.build',
-            MONGO_URL: 'mongodb://prossimo-user:prossimo2018@ds113700.mlab.com:13700/prossimo'
+            MONGO_URL: 'mongodb://localhost:27017/prossimo-prod'
         },
 
         buildOptions: {
@@ -44,5 +44,13 @@ module.exports = {
         deployCheckWaitTime: 60,
 
         enableUploadProgressBar: true
+    },
+
+    mongo: {
+        port: 27017,
+        version: '3.4.1',
+        servers: {
+            one: {}
+        }
     }
 }
