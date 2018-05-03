c483bd0b0ef51ac86e696441c417120b3d2258a0
Ismail Frolov <i.f.0109@hotmail.com>
2018-05-03 06:41:23 +0300

    Change mongo_url on mup-stage

--- mup-stage.js
+++ mup-stage.js
@@ -24,7 +24,7 @@ module.exports = {
 
         env: {
             ROOT_URL: 'https://crm-test.mavrik.build',
-            MONGO_URL: 'mongodb://localhost:27017/prossimo-stage'
+            MONGO_URL: 'mongodb://prossimo-user:prossimo2018@ds113700.mlab.com:13700/prossimo'
         },
 
         buildOptions: {
