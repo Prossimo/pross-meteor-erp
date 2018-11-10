2b9de61910b6f241516a3db029b05bde6f3088dd
Mes <messarg@gmail.com>
2018-11-10 16:47:06 +0400

    Closes #1139: cannot edit text in task card description

--- imports/ui/components/tasks/TaskDetail.jsx
+++ imports/ui/components/tasks/TaskDetail.jsx
@@ -1,25 +1,25 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { FlowRouter } from 'meteor/kadira:flow-router'
-import { Modal } from 'react-bootstrap'
-import DatePicker from 'react-datepicker'
-import moment from 'moment'
-import SelectUser from './SelectUser'
-import TextEditor from './TextEditor'
-import TaskName from './TaskName'
-import TaskError from './TaskError'
-import TaskComment from './TaskComment'
-import UploadFrom from './upload/UploadFrom'
-import UploadOverlay from './upload/UploadOverlay'
-import Attachments from './upload/Attachments'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { FlowRouter } from "meteor/kadira:flow-router";
+import { Modal } from "react-bootstrap";
+import DatePicker from "react-datepicker";
+import moment from "moment";
+import SelectUser from "./SelectUser";
+import TextEditor from "./TextEditor";
+import TaskName from "./TaskName";
+import TaskError from "./TaskError";
+import TaskComment from "./TaskComment";
+import UploadFrom from "./upload/UploadFrom";
+import UploadOverlay from "./upload/UploadOverlay";
+import Attachments from "./upload/Attachments";
 
 class TaskDetail extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       isFinding: {
         assignee: false,
-        approver: false,
+        approver: false
       },
       errors: [],
       isAttach: false,
@@ -27,192 +27,246 @@ class TaskDetail extends Component {
         name: `Task #${props.total + 1}`,
         assignee: null,
         approver: null,
-        dueDate: new Date,
-        description: '',
-        status: props.status,
+        dueDate: new Date(),
+        description: "",
+        status: props.status
       }
-    }
+    };
 
-    this.changeState = this.changeState.bind(this)
-    this.saveTask = this.saveTask.bind(this)
-    this.assignToMe = this.assignToMe.bind(this)
+    this.changeState = this.changeState.bind(this);
+    this.saveTask = this.saveTask.bind(this);
+    this.assignToMe = this.assignToMe.bind(this);
   }
 
   assignToMe() {
-    const _id = this.props.task._id
-    Meteor.call('task.assignToMe', { _id })
+    const _id = this.props.task._id;
+    Meteor.call("task.assignToMe", { _id });
   }
 
   saveTask() {
-    const parentId = FlowRouter.current().params.id
-    const parentType = FlowRouter.current().route.name.toLowerCase()
-    const { name, assignee, approver, dueDate, description, status } = this.state.task
+    const parentId = FlowRouter.current().params.id;
+    const parentType = FlowRouter.current().route.name.toLowerCase();
+    const {
+      name,
+      assignee,
+      approver,
+      dueDate,
+      description,
+      status
+    } = this.state.task;
     const task = {
       name,
-      assignee: assignee ? assignee._id : '',
-      approver: approver ? approver._id : '',
+      assignee: assignee ? assignee._id : "",
+      approver: approver ? approver._id : "",
       description,
-      dueDate: moment(`${moment(dueDate).format('YYYY-MM-DD')} 23:59:59`).toDate(),
+      dueDate: moment(
+        `${moment(dueDate).format("YYYY-MM-DD")} 23:59:59`
+      ).toDate(),
       status,
       parentId,
       parentType
-    }
+    };
 
     if (this.props.isNew) {
-      Meteor.call('task.create', task, error => {
+      Meteor.call("task.create", task, error => {
         if (error) {
-          const msg = error.reason ? error.reason : error.message
-          this.setState({ errors: [msg] })
+          const msg = error.reason ? error.reason : error.message;
+          this.setState({ errors: [msg] });
         } else {
-          this.props.hideDetail()
-          this.setState({ errors: [] })
+          this.props.hideDetail();
+          this.setState({ errors: [] });
         }
-      })
+      });
     } else {
-      task._id = this.props.task._id
-      Meteor.call('task.update', task, error => {
+      task._id = this.props.task._id;
+      Meteor.call("task.update", task, error => {
         if (error) {
-          const msg = error.reason ? error.reason : error.message
-          this.setState({ errors: [msg] })
+          const msg = error.reason ? error.reason : error.message;
+          this.setState({ errors: [msg] });
         } else {
-          this.props.hideDetail()
-          this.setState({ errors: [] })
+          this.props.hideDetail();
+          this.setState({ errors: [] });
         }
-      })
+      });
     }
   }
 
   changeState(prop, propName, propValue) {
-    prop[propName] = propValue
-    this.setState(prevState => prevState)
+    prop[propName] = propValue;
+    this.setState(prevState => prevState);
   }
 
   render() {
     const selectUsers = [
       {
-        name: 'assignee',
+        name: "assignee",
         top: 50,
-        ignore: 'approver',
-        label: 'Assignee',
+        ignore: "approver",
+        label: "Assignee"
       },
       {
-        name: 'approver',
+        name: "approver",
         top: 50,
-        ignore: 'assignee',
-        label: 'Approver',
-      },
-    ]
-    let hasAssignToMe = false
+        ignore: "assignee",
+        label: "Approver"
+      }
+    ];
+    let hasAssignToMe = false;
     if (this.props.task && !this.props.isNew) {
-      hasAssignToMe = !this.props.task.assignee || (this.props.task.assignee && this.props.task.assignee._id && this.props.task.assignee._id !== Meteor.userId())
+      hasAssignToMe =
+        !this.props.task.assignee ||
+        (this.props.task.assignee &&
+          this.props.task.assignee._id &&
+          this.props.task.assignee._id !== Meteor.userId());
     }
     return (
       <Modal
-        dialogClassName='task-details'
+        dialogClassName="task-details"
         show={this.props.isShown}
         onHide={this.props.hideDetail}
       >
         <Modal.Body>
-          <div className='row'>
-
-            <div className='col-md-9'>
+          <div className="row">
+            <div className="col-md-9">
               <TaskName
                 name={this.state.task.name}
                 isNew={this.props.isNew}
-                onChange={name => { this.state.task.name = name }}
+                onChange={name => {
+                  this.state.task.name = name;
+                }}
               />
               <TextEditor
                 content={this.state.task.description}
-                onChange={description => { this.state.task.description = description }}
+                onChange={description => {
+                  this.state.task.description = description;
+                }}
               />
             </div>
 
-            <div className='col-md-3'>
-              {
-                selectUsers.map(({ name, top, label, ignore }) =>
-                  (
-                    <SelectUser
-                      key={name}
-                      isFinding={this.state.isFinding[name]}
-                      closeFinding={() => this.changeState(this.state.isFinding, name, false)}
-                      ignoreUser={this.state.task[ignore]}
-                      title={label}
-                      top={top}
-                      user={this.state.task[name]}
-                      onSelectUser={(user) => this.changeState(this.state.task, name, user)}
-                      removeUser={() => this.changeState(this.state.task, name, null)}
-                      toggleFinding={() => this.changeState(this.state.isFinding, name, !this.state.isFinding[name])}
-                    />
-                  )
-                )
-              }
-              <div className='form-group'>
-                <label style={{fontSize: '14px'}}>Due Date</label>
-                <div className='form-control'>
+            <div className="col-md-3">
+              {selectUsers.map(({ name, top, label, ignore }) => (
+                <SelectUser
+                  key={name}
+                  isFinding={this.state.isFinding[name]}
+                  closeFinding={() =>
+                    this.changeState(this.state.isFinding, name, false)
+                  }
+                  ignoreUser={this.state.task[ignore]}
+                  title={label}
+                  top={top}
+                  user={this.state.task[name]}
+                  onSelectUser={user =>
+                    this.changeState(this.state.task, name, user)
+                  }
+                  removeUser={() =>
+                    this.changeState(this.state.task, name, null)
+                  }
+                  toggleFinding={() =>
+                    this.changeState(
+                      this.state.isFinding,
+                      name,
+                      !this.state.isFinding[name]
+                    )
+                  }
+                />
+              ))}
+              <div className="form-group">
+                <label style={{ fontSize: "14px" }}>Due Date</label>
+                <div className="form-control">
                   <DatePicker
                     selected={moment(this.state.task.dueDate)}
                     minDate={moment()}
-                    onChange={date => this.changeState(this.state.task, 'dueDate', date.toDate())}
+                    onChange={date =>
+                      this.changeState(
+                        this.state.task,
+                        "dueDate",
+                        date.toDate()
+                      )
+                    }
                   />
                 </div>
               </div>
-              <div className='form-group'>
-                {
-                  (!this.props.isNew) ? (
-                    <button
-                      className='btn btn-default full-width hide'
-                      onClick={() => this.changeState(this.state, 'isAttach', !this.state.isAttach)}>
-                      <i className='fa fa-paperclip'/>
-                      <small> Attachment</small>
-                    </button>
-                  ) : ''
-                }
-                {
-                  (this.state.isAttach) ? (
-                    <UploadFrom taskFolderId={this.props.taskFolderId} taskId={this.props.task._id} close={() => this.changeState(this.state, 'isAttach', false)} className='hide'/>
-                  ) : ''
-                }
+              <div className="form-group">
+                {!this.props.isNew ? (
+                  <button
+                    className="btn btn-default full-width hide"
+                    onClick={() =>
+                      this.changeState(
+                        this.state,
+                        "isAttach",
+                        !this.state.isAttach
+                      )
+                    }
+                  >
+                    <i className="fa fa-paperclip" />
+                    <small> Attachment</small>
+                  </button>
+                ) : (
+                  ""
+                )}
+                {this.state.isAttach ? (
+                  <UploadFrom
+                    taskFolderId={this.props.taskFolderId}
+                    taskId={this.props.task._id}
+                    close={() =>
+                      this.changeState(this.state, "isAttach", false)
+                    }
+                    className="hide"
+                  />
+                ) : (
+                  ""
+                )}
               </div>
-              {
-                (hasAssignToMe) ? (
-                  <div className='form-group'>
-                    <button className='btn btn-warning full-width' onClick={ this.assignToMe }>Assign to me</button>
-                  </div>
-                ) : ''
-              }
-              <div className='form-group'>
-                <button className='btn btn-primary full-width' onClick={ this.saveTask }>Save</button>
+              {hasAssignToMe ? (
+                <div className="form-group">
+                  <button
+                    className="btn btn-warning full-width"
+                    onClick={this.assignToMe}
+                  >
+                    Assign to me
+                  </button>
+                </div>
+              ) : (
+                ""
+              )}
+              <div className="form-group">
+                <button
+                  className="btn btn-primary full-width"
+                  onClick={this.saveTask}
+                >
+                  Save
+                </button>
               </div>
             </div>
-            <div className='col-md-12'>
-              <TaskError errors={ this.state.errors }/>
+            <div className="col-md-12">
+              <TaskError errors={this.state.errors} />
             </div>
-            <div className='col-md-12'>
-              {
-                (!this.props.isNew) ? (
-                  <Attachments attachments={this.props.task.attachments || []} taskId={this.props.task._id} />
-                ) : ''
-              }
+            <div className="col-md-12">
+              {!this.props.isNew ? (
+                <Attachments
+                  attachments={this.props.task.attachments || []}
+                  taskId={this.props.task._id}
+                />
+              ) : (
+                ""
+              )}
             </div>
-            <div className='col-md-12'>
-              {
-                (!this.props.isNew) ? (
-                  <TaskComment task={this.props.task}/>
-                ) : ''
-              }
+            <div className="col-md-12">
+              {!this.props.isNew ? <TaskComment task={this.props.task} /> : ""}
             </div>
           </div>
-              {
-                (!this.props.isNew) ? (
-                  <UploadOverlay
-                    taskFolderId={this.props.taskFolderId}
-                    taskId={this.props.task._id}/>
-                ) : ''
-              }
+          {!this.props.isNew ? (
+            <UploadOverlay
+              taskFolderId={this.props.taskFolderId}
+              taskId={this.props.task._id}
+            />
+          ) : (
+            ""
+          )}
         </Modal.Body>
       </Modal>
-    )
-  };
+    );
+  }
 }
 
 TaskDetail.propTypes = {
@@ -223,7 +277,7 @@ TaskDetail.propTypes = {
   isNew: PropTypes.bool.isRequired,
   task: PropTypes.object,
   taskFolderId: PropTypes.string,
-  total: PropTypes.number.isRequired,
-}
+  total: PropTypes.number.isRequired
+};
 
-export default TaskDetail
+export default TaskDetail;
--- imports/ui/components/tasks/TextEditor.jsx
+++ imports/ui/components/tasks/TextEditor.jsx
@@ -1,20 +1,24 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import styled from 'styled-components'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
 
 class TextEditor extends Component {
   constructor(props) {
-    super(props)
-    this.changeContent = this.changeContent.bind(this)
+    super(props);
+    this.state = {
+      inputFocus: false
+    };
+    this.changeContent = this.changeContent.bind(this);
   }
 
   changeContent(event) {
-    const content = event.target.value
-    this.props.onChange(content)
+    const content = event.target.value;
+    this.props.onChange(content);
   }
 
   render() {
-    const TextArea = styled.textarea `
+    console.log("inputFocus=====", this.state.inputFocus);
+    const TextArea = styled.textarea`
       font-size: 14px;
       margin-top: 15px;
       margin-left: 15px;
@@ -26,33 +30,36 @@ class TextEditor extends Component {
       border-width: medium 1px 1px medium;
       box-shadow: 0 1px 2px rgba(0, 0, 0, 0.12) inset;
       color: #555555;
-      font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;
+      font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
       font-size: 1em;
       line-height: 1.4em;
       padding: 5px 8px;
       transition: background-color 0.2s ease 0s;
-      background: none repeat scroll 0 0 #FFFFFF;
+      background: none repeat scroll 0 0 #ffffff;
       border-left-color: green;
       resize: none;
       &:focus {
         background: none repeat scroll 0 0 rgba(0, 0, 0, 0.07);
         outline-width: 0;
       }
-    `
+    `;
     return (
       <TextArea
-        ref='content'
-        placeholder='This task is about ...'
+        ref="content"
+        autoFocus={this.state.inputFocus}
+        placeholder="This task is about ..."
         defaultValue={this.props.content}
-        onChange={ this.changeContent }
+        onClick={() => this.setState({ inputFocus: true })}
+        onBlur={() => this.setState({ inputFocus: false })}
+        onChange={this.changeContent}
       />
-    )
+    );
   }
 }
 
 TextEditor.propTypes = {
   content: PropTypes.string,
-  onChange: PropTypes.func.isRequired,
-}
+  onChange: PropTypes.func.isRequired
+};
 
-export default TextEditor
+export default TextEditor;
