009186d28b2b6ee627ad5fead94cded38e858f11
Aram Bayadyan <aram.bv@gmail.com>
2018-11-16 21:37:07 +0400

    Closes #1144: emailview link not working for individual messages

--- imports/api/models/messages/publications.js
+++ imports/api/models/messages/publications.js
@@ -1,114 +1,116 @@
-import _ from 'underscore'
-import {Meteor} from 'meteor/meteor'
-import {Roles} from 'meteor/alanning:roles'
-import {ROLES} from '../users/users'
-import Messages from './messages'
-import SalesRecords from '../salesRecords/salesRecords'
-import Projects from '../projects/projects'
-import Conversations from '../conversations/conversations'
-import Threads from '../threads/threads'
+import _ from "underscore";
+import { Meteor } from "meteor/meteor";
+import { Roles } from "meteor/alanning:roles";
+import { ROLES } from "../users/users";
+import Messages from "./messages";
+import SalesRecords from "../salesRecords/salesRecords";
+import Projects from "../projects/projects";
+import Conversations from "../conversations/conversations";
+import Threads from "../threads/threads";
 
-Meteor.publish('messages.mine', function() {
-    if(!this.userId) {
-        this.ready()
-        return
+Meteor.publish("messages.mine", function() {
+  if (!this.userId) {
+    this.ready();
+    return;
+  }
+
+  if (Roles.userIsInRole(this.userId, ROLES.ADMIN)) {
+    return Messages.find();
+  }
+
+  const nylasAccounts = Meteor.users
+    .findOne({ _id: this.userId })
+    .nylasAccounts();
+  return Messages.find({
+    account_id: { $in: _.pluck(nylasAccounts, "accountId") }
+  });
+});
+
+Meteor.publish("messages.one", function({ threadId, messageId }) {
+  check(threadId, Match.Maybe(String));
+  check(messageId, Match.Maybe(String));
+
+  if (!this.userId) {
+    this.ready();
+    return;
+  }
+
+  return Messages.find({ id: messageId });
+});
+
+Meteor.publish("messages.all", function() {
+  if (!this.userId) return this.ready();
+
+  return Messages.find();
+});
+
+Meteor.publish("messages.bySalesRecord", function(salesRecordId) {
+  check(salesRecordId, String);
+
+  if (!this.userId) return this.ready();
+
+  const salesRecord = SalesRecords.findOne(salesRecordId);
+
+  if (salesRecord) {
+    const threads = salesRecord.threads();
+
+    if (threads && threads.length > 0) {
+      return Messages.find({ thread_id: _.pluck(threads, "id") });
     }
+  }
+  return this.ready();
+});
 
-    if(Roles.userIsInRole(this.userId, ROLES.ADMIN)) {
-        return Messages.find()
+Meteor.publish("messages.byProject", function(projectId) {
+  check(projectId, String);
+
+  if (!this.userId) return this.ready();
+
+  const project = Projects.findOne(projectId);
+
+  if (project) {
+    const threads = project.threads();
+
+    if (threads && threads.length > 0) {
+      return Messages.find({ thread_id: _.pluck(threads, "id") });
     }
+  }
+  return this.ready();
+});
+Meteor.publish("messages.byThread", function(threadId) {
+  check(threadId, String);
 
-    const nylasAccounts = Meteor.users.findOne({_id:this.userId}).nylasAccounts()
-    return Messages.find({account_id:{$in:_.pluck(nylasAccounts, 'accountId')}})
-})
+  if (!this.userId) return this.ready();
 
+  return Messages.find({ thread_id: threadId });
+});
 
-Meteor.publish('messages.one', function({threadId, messageId}) {
-    check(threadId, Match.Maybe(String))
-    check(messageId, Match.Maybe(String))
+Meteor.publish("messages.params", function(filters = {}, options = {}) {
+  check(filters, Object);
+  check(options, {
+    sort: Match.Maybe(Object),
+    skip: Match.Maybe(Number),
+    limit: Match.Maybe(Number)
+  });
 
-    if(!this.userId) {
-        this.ready()
-        return
-    }
+  if (!this.userId) {
+    this.ready();
+    return;
+  }
 
-    return Messages.find({'$or':[{id:messageId}, {thread_id:threadId}]})
-})
+  //if(!options.skip) options.skip = 0
+  //if(!options.limit) options.limit = 100
+  //console.log('threads.params publication', filters, options, Threads.find(filters, options).fetch().length)
+  return Messages.find(filters, options);
+});
 
+Meteor.publish("messages.custom", function(query, options) {
+  check(query, Object);
+  check(options, Object);
+  if (!this.userId) {
+    this.ready();
+    return;
+  }
 
-Meteor.publish('messages.all', function () {
-    if(!this.userId) return this.ready()
-    
-    return Messages.find()
-})
-
-Meteor.publish('messages.bySalesRecord', function (salesRecordId) {
-    check(salesRecordId, String)
-
-    if(!this.userId) return this.ready()
-
-    const salesRecord = SalesRecords.findOne(salesRecordId)
-
-    if(salesRecord) {
-        const threads = salesRecord.threads()
-
-        if(threads && threads.length>0) {
-            return Messages.find({thread_id: _.pluck(threads, 'id')})
-        }
-    }
-    return this.ready()
-})
-
-Meteor.publish('messages.byProject', function (projectId) {
-    check(projectId, String)
-
-    if(!this.userId) return this.ready()
-
-    const project = Projects.findOne(projectId)
-
-    if(project) {
-        const threads = project.threads()
-
-        if(threads && threads.length>0) {
-            return Messages.find({thread_id: _.pluck(threads, 'id')})
-        }
-    }
-    return this.ready()
-})
-Meteor.publish('messages.byThread', function (threadId) {
-    check(threadId, String)
-
-    if(!this.userId) return this.ready()
-
-    return Messages.find({thread_id: threadId})
-})
-
-Meteor.publish('messages.params', function(filters={}, options={}) {
-   check(filters, Object)
-   check(options, {
-      sort: Match.Maybe(Object),
-      skip: Match.Maybe(Number),
-      limit: Match.Maybe(Number)
-   })
-
-   if(!this.userId) {
-      this.ready()
-      return
-   }
-
-   //if(!options.skip) options.skip = 0
-   //if(!options.limit) options.limit = 100
-   //console.log('threads.params publication', filters, options, Threads.find(filters, options).fetch().length)
-   return Messages.find(filters, options)
-})
-
-Meteor.publish('messages.custom', function(query, options) {
-    check(query, Object)
-    check(options, Object)
-    if (!this.userId) {
-        this.ready()
-        return
-    }
-
-    return Messages.find(query, options)
-})
+  return Messages.find(query, options);
+});
--- imports/ui/components/inbox/MessageParticipants.js
+++ imports/ui/components/inbox/MessageParticipants.js
@@ -1,194 +1,205 @@
-import _ from 'underscore';
-import React from 'react'
-import PropTypes from 'prop-types';
-import classnames from 'classnames';
-import NylasUtils from '../../../api/nylas/nylas-utils';
+import _ from "underscore";
+import React from "react";
+import PropTypes from "prop-types";
+import classnames from "classnames";
+import NylasUtils from "../../../api/nylas/nylas-utils";
 
 const MAX_COLLAPSED = 5;
 
 class MessageParticipants extends React.Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
+    this.selectText = this.selectText.bind(this);
+  }
 
-        this.selectText = this.selectText.bind(this);
+  allToParticipants() {
+    return _.union(this.props.to, this.props.cc, this.props.bcc);
+  }
+
+  shortNames(participants = [], max = MAX_COLLAPSED) {
+    let names = NylasUtils.getParticipantsNamesArray(participants, false);
+    if (names.length > max) {
+      extra = names.length - max;
+      names = names.slice(0, max);
+      names.push(`and ${extra} more`);
     }
+    return names.join(", ");
+  }
 
+  selectText(e) {
+    textNode = e.currentTarget.childNodes[0];
 
-    allToParticipants() {
-        return _.union(this.props.to, this.props.cc, this.props.bcc);
-    }
+    range = document.createRange();
+    range.setStart(textNode, 0);
+    range.setEnd(textNode, textNode.length);
+    selection = document.getSelection();
+    selection.removeAllRanges();
+    selection.addRange(range);
+  }
 
-    shortNames(participants = [], max = MAX_COLLAPSED) {
-        let names = NylasUtils.getParticipantsNamesArray(participants, false);
-        if (names.length > max) {
-            extra = names.length - max;
-            names = names.slice(0, max);
-            names.push(`and ${extra} more`);
-        }
-        return names.join(", ");
-    }
+  render() {
+    const classSet = classnames({
+      participants: true,
+      "message-participants": true,
+      collapsed: !this.props.isDetailed,
+      "from-participants": this.props.from.length > 0,
+      "to-participants": this.allToParticipants().length > 0
+    });
 
-    selectText(e) {
-        textNode = e.currentTarget.childNodes[0]
+    return (
+      <div className={classSet} onClick={this.props.onClick}>
+        {this.props.isDetailed ? this.renderExpanded() : this.renderCollapsed()}
+      </div>
+    );
+  }
 
-        range = document.createRange()
-        range.setStart(textNode, 0)
-        range.setEnd(textNode, textNode.length)
-        selection = document.getSelection()
-        selection.removeAllRanges()
-        selection.addRange(range)
-    }
+  renderExpanded() {
+    let expanded = [];
 
-
-    render() {
-        const classSet = classnames({
-            "participants": true,
-            "message-participants": true,
-            "collapsed": !this.props.isDetailed,
-            "from-participants": this.props.from.length > 0,
-            "to-participants": this.allToParticipants().length > 0
+    if (this.props.from.length > 0) {
+      expanded.push(
+        this.renderExpandedField("from", this.props.from, {
+          includeLabel: false
         })
+      );
+    }
+    if (this.props.to.length > 0) {
+      expanded.push(this.renderExpandedField("to", this.props.to));
+    }
+    if (this.props.cc.length > 0) {
+      expanded.push(this.renderExpandedField("cc", this.props.cc));
+    }
+    if (this.props.bcc.length > 0) {
+      expanded.push(this.renderExpandedField("bcc", this.props.bcc));
+    }
 
+    return <div className="expanded-participants">{expanded}</div>;
+  }
+
+  renderFullParticipants(participants = []) {
+    _.map(participants, (p, i) => {
+      let comma;
+      if (participants.length == 1) comma = "";
+      else if (i == participants.length - 1) comma = "";
+      else comma = ",";
+
+      if (p.name && p.name.length > 0 && p.name != p.email)
         return (
-            <div className={classSet} onClick={this.props.onClick}>
-                {this.props.isDetailed ? this.renderExpanded() : this.renderCollapsed()}
+          <div key={`${p.email}-${i}`} className="participant selectable">
+            <div className="participant-primary" onClick={this.selectText}>
+              {p.name}
             </div>
-        )
-    }
-
-    renderExpanded() {
-        let expanded = [];
-
-        if (this.props.from.length > 0) {
-            expanded.push(this.renderExpandedField('from', this.props.from, {includeLabel: false}));
-        }
-        if (this.props.to.length > 0) {
-            expanded.push(this.renderExpandedField('to', this.props.to));
-        }
-        if (this.props.cc.length > 0) {
-            expanded.push(this.renderExpandedField('cc', this.props.cc));
-        }
-        if (this.props.bcc.length > 0) {
-            expanded.push(this.renderExpandedField('bcc', this.props.bcc));
-        }
-
-        return (
-            <div className="expanded-participants">
-                {expanded}
+            <div className="participant-secondary">
+              {"<"}
+              <span onClick={this.selectText}>{p.email}</span>
+              {`>${comma}`}
             </div>
-        )
-    }
-
-    renderFullParticipants(participants = []) {
-        _.map(participants, (p, i) => {
-
-            if (participants.length == 1) comma = "";
-            else if (i == participants.length - 1) comma = "";
-            else comma = ","
-
-            if (p.name && p.name.length > 0 && p.name != p.email)
-                return (
-                    <div key={`${p.email}-${i}`} className="participant selectable">
-                        <div className="participant-primary" onClick={this.selectText}>
-                            {p.name}
-                        </div>
-                        <div className="participant-secondary">
-                            {"<"}<span onClick={this.selectText}>{p.email}</span>{`>${comma}`}
-                        </div>
-                    </div>
-                )
-            else
-                return (
-                    <div key={`${p.email}-${i}`} className="participant selectable">
-                        <div className="participant-primary">
-                            <span onClick={this.selectText}>{p.email}</span>{comma}
-                        </div>
-                    </div>
-                )
-        })
-    }
-
-    renderExpandedField(name, field, {includeLabel} = {}) {
-        includeLabel = includeLabel || true;
-
+          </div>
+        );
+      else
         return (
-            <div className="participant-type" key={`participant-type-${name}`}>
-                {includeLabel ? <div className={`participant-label ${name}-label`}>{name}:&nbsp;</div> : undefined}
-                <div className={`participant-name ${name}-contact`}>
-                    {this.renderFullContacts(field)}
-                </div>
+          <div key={`${p.email}-${i}`} className="participant selectable">
+            <div className="participant-primary">
+              <span onClick={this.selectText}>{p.email}</span>
+              {comma}
             </div>
-        )
-    }
+          </div>
+        );
+    });
+  }
 
-    renderFullContacts(contacts = []) {
-        return _.map(contacts, (c, i) => {
-            if (contacts.length == 1) comma = ""
-            else if (i == contacts.length - 1) comma = ""
-            else comma = ","
+  renderExpandedField(name, field, { includeLabel } = {}) {
+    includeLabel = includeLabel || true;
 
-            if (c.name && c.name.length && c.name != c.email) {
-                return (
-                    <div key={`${c.email}-${i}`} className="participant selectable">
-                        <div className="participant-primary" onClick={this.selectText}>
-                            {c.name}
-                        </div>
-                        <div className="participant-secondary">
-                            {"<"}<span onClick={this.selectText}>{c.email}</span>{`>${comma}`}
-                        </div>
-                    </div>
-                )
-            } else {
-                return (
-                    <div key={`${c.email}-${i}`} className="participant selectable">
-                        <div className="participant-primary">
-                            <span onClick={this.selectText}>{c.email}</span>{comma}
-                        </div>
-                    </div>
-                )
-            }
-        });
-    }
+    return (
+      <div className="participant-type" key={`participant-type-${name}`}>
+        {includeLabel ? (
+          <div className={`participant-label ${name}-label`}>{name}:&nbsp;</div>
+        ) : (
+          undefined
+        )}
+        <div className={`participant-name ${name}-contact`}>
+          {this.renderFullContacts(field)}
+        </div>
+      </div>
+    );
+  }
 
-    renderCollapsed() {
-        let childSpans = [];
-        let toParticipants = this.allToParticipants();
+  renderFullContacts(contacts = []) {
+    return _.map(contacts, (c, i) => {
+      let comma;
+      if (contacts.length == 1) comma = "";
+      else if (i == contacts.length - 1) comma = "";
+      else comma = ",";
 
-        if (this.props.from.length > 0) {
-            childSpans.push(<span className="participant-name from-contact"
-                                  key="from">{this.shortNames(this.props.from)}</span>)
-        }
-
-        if (toParticipants.length > 0) {
-            childSpans.push([
-                <span className="participant-label to-label" key="to-label">To:&nbsp;</span>,
-                <span className="participant-name to-contact" key="to-value">{this.shortNames(toParticipants)}</span>
-            ])
-        }
-
+      if (c.name && c.name.length && c.name != c.email) {
         return (
-            <span className="collapsed-participants">
-                {childSpans}
-            </span>
-        )
+          <div key={`${c.email}-${i}`} className="participant selectable">
+            <div className="participant-primary" onClick={this.selectText}>
+              {c.name}
+            </div>
+            <div className="participant-secondary">
+              {"<"}
+              <span onClick={this.selectText}>{c.email}</span>
+              {`>${comma}`}
+            </div>
+          </div>
+        );
+      } else {
+        return (
+          <div key={`${c.email}-${i}`} className="participant selectable">
+            <div className="participant-primary">
+              <span onClick={this.selectText}>{c.email}</span>
+              {comma}
+            </div>
+          </div>
+        );
+      }
+    });
+  }
+
+  renderCollapsed() {
+    let childSpans = [];
+    let toParticipants = this.allToParticipants();
+
+    if (this.props.from.length > 0) {
+      childSpans.push(
+        <span className="participant-name from-contact" key="from">
+          {this.shortNames(this.props.from)}
+        </span>
+      );
     }
+
+    if (toParticipants.length > 0) {
+      childSpans.push([
+        <span className="participant-label to-label" key="to-label">
+          To:&nbsp;
+        </span>,
+        <span className="participant-name to-contact" key="to-value">
+          {this.shortNames(toParticipants)}
+        </span>
+      ]);
+    }
+
+    return <span className="collapsed-participants">{childSpans}</span>;
+  }
 }
 
 MessageParticipants.propTypes = {
-    to: PropTypes.array,
-    cc: PropTypes.array,
-    bcc: PropTypes.array,
-    from: PropTypes.array,
-    onClick: PropTypes.func,
-    isDetailed: PropTypes.bool
-}
+  to: PropTypes.array,
+  cc: PropTypes.array,
+  bcc: PropTypes.array,
+  from: PropTypes.array,
+  onClick: PropTypes.func,
+  isDetailed: PropTypes.bool
+};
 
 MessageParticipants.defaultProps = {
-    to: [],
-    cc: [],
-    bcc: [],
-    from: []
-}
+  to: [],
+  cc: [],
+  bcc: [],
+  from: []
+};
 
 export default MessageParticipants;
