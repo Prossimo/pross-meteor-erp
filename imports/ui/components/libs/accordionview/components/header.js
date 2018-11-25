39bb941d782d941df961e0b3b6c8ba8f4f2ea5b0
Aram Bayadyan <aram.bv@gmail.com>
2018-11-25 22:41:55 +0400

    stylus -> scss conversion
    removed stylus package

--- imports/ui/components/libs/accordionview/components/header.js
+++ imports/ui/components/libs/accordionview/components/header.js
@@ -1,55 +1,55 @@
-'use strict';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import shallowEqual from "shallowequal";
+import deepEqual from "deep-equal";
 
-import React from 'react'
-import PropTypes from 'prop-types';
-import shallowEqual from 'shallowequal';
-import deepEqual from 'deep-equal';
-
-class NodeHeader extends React.Component {
-    constructor(props){
-        super(props);
+class NodeHeader extends Component {
+  constructor(props) {
+    super(props);
+  }
+  shouldComponentUpdate(nextProps) {
+    const props = this.props;
+    const nextPropKeys = Object.keys(nextProps);
+    for (let i = 0; i < nextPropKeys.length; i++) {
+      const key = nextPropKeys[i];
+      if (key === "animations") {
+        continue;
+      }
+      const isEqual = shallowEqual(props[key], nextProps[key]);
+      if (!isEqual) {
+        return true;
+      }
     }
-    shouldComponentUpdate(nextProps){
-        const props = this.props;
-        const nextPropKeys = Object.keys(nextProps);
-        for(let i = 0; i < nextPropKeys.length; i++){
-            const key = nextPropKeys[i];
-            if(key === 'animations'){ continue; }
-            const isEqual = shallowEqual(props[key], nextProps[key]);
-            if(!isEqual){ return true; }
-        }
-        return !deepEqual(props.animations, nextProps.animations, { strict: true });
-    }
-    render(){
-        const {style, decorators} = this.props;
-        const terminal = !this.props.node.children;
-        const active = this.props.node.active;
-        const container = [style.link, active ? style.activeLink : null];
-        const headerStyles = Object.assign({ container }, this.props.style);
-        return (
-            <decorators.Container
-                style={headerStyles}
-                decorators={decorators}
-                terminal={terminal}
-                onClick={this.props.onClick}
-                animations={this.props.animations}
-                node={this.props.node}
-                removeFile={this.props.removeFile}
-            />
-        );
-    }
+    return !deepEqual(props.animations, nextProps.animations, { strict: true });
+  }
+  render() {
+    const { style, decorators } = this.props;
+    const terminal = !this.props.node.children;
+    const active = this.props.node.active;
+    const container = [style.link, active ? style.activeLink : null];
+    const headerStyles = Object.assign({ container }, this.props.style);
+    return (
+      <decorators.Container
+        style={headerStyles}
+        decorators={decorators}
+        terminal={terminal}
+        onClick={this.props.onClick}
+        animations={this.props.animations}
+        node={this.props.node}
+        removeFile={this.props.removeFile}
+      />
+    );
+  }
 }
 
 NodeHeader.propTypes = {
-    style: PropTypes.object.isRequired,
-    decorators: PropTypes.object.isRequired,
-    animations: PropTypes.oneOfType([
-        PropTypes.object,
-        PropTypes.bool
-    ]).isRequired,
-    node: PropTypes.object.isRequired,
-    onClick: PropTypes.func,
-    removeFile: PropTypes.func
+  style: PropTypes.object.isRequired,
+  decorators: PropTypes.object.isRequired,
+  animations: PropTypes.oneOfType([PropTypes.object, PropTypes.bool])
+    .isRequired,
+  node: PropTypes.object.isRequired,
+  onClick: PropTypes.func,
+  removeFile: PropTypes.func
 };
 
 export default NodeHeader;
