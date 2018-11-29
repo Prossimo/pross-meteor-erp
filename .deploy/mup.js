582867bc9fe9d910a1affc21d838fd46da61143e
Aram Bayadyan <aram.bv@gmail.com>
2018-11-29 04:39:48 +0400

    replaced abernix js minifier and mongo-oplog

--- .deploy/mup.js
+++ .deploy/mup.js
@@ -21,18 +21,17 @@ module.exports = {
 
     buildOptions: {
       serverOnly: true,
-      debug: true,
+      debug: false,
       cleanAfterBuild: true
     },
 
     env: {
       // TODO: Change to your app's url
       // If you are using ssl, it needs to start with https://
+      NODE_ENV: "production",
       ROOT_URL: "https://crossdevs.com",
-      MONGO_URL:
-        "mongodb://mavrik_user:P4ssiveH0use@ds115580-a0.mlab.com:15580,ds115580-a1.mlab.com:15580/mavrikprod?replicaSet=rs-ds115580",
-      MONGO_OPLOG_URL:
-        "mongodb://oplog-reader:P4ssiveH0use@ds115580-a0.mlab.com:15580,ds115580-a1.mlab.com:15580/local?replicaSet=rs-ds115580&authSource=admin"
+      MONGO_URL: "mongodb://mongodb/meteor",
+      MONGO_OPLOG_URL: "mongodb://mongodb/local"
     },
 
     docker: {
@@ -45,12 +44,12 @@ module.exports = {
     enableUploadProgressBar: true
   },
 
-  // mongo: {
-  //   version: "3.4.1",
-  //   servers: {
-  //     one: {}
-  //   }
-  // },
+  mongo: {
+    version: "3.4.1",
+    servers: {
+      one: {}
+    }
+  },
 
   // (Optional)
   // Use the proxy to setup ssl or to route requests to the correct
